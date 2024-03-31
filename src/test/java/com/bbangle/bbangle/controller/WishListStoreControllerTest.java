0c0b533627747de7fcce7b1f1faa5ed75c197399
sikdong <zmstnv1119@naver.com>
2024-03-31 09:28:29 +0900

    refactor : 기존 WebMvcTest 시 Repository 탐색 오류로 인한 SpringBootTest로 전환

--- src/test/java/com/bbangle/bbangle/controller/WishListStoreControllerTest.java
+++ src/test/java/com/bbangle/bbangle/controller/WishListStoreControllerTest.java
@@ -1,79 +1,49 @@
 package com.bbangle.bbangle.controller;
 
-import com.bbangle.bbangle.config.CorsConfig;
-import com.bbangle.bbangle.config.QueryDslConfig;
-import com.bbangle.bbangle.config.WebOAuthSecurityConfig;
-import com.bbangle.bbangle.configuration.TestSecurityConfig;
-import com.bbangle.bbangle.member.repository.MemberRepository;
-import com.bbangle.bbangle.wishListFolder.repository.WishListFolderRepository;
-import com.bbangle.bbangle.wishListFolder.repository.WishListFolderRepositoryImpl;
 import com.bbangle.bbangle.wishListStore.controller.WishListStoreController;
-import com.bbangle.bbangle.wishListStore.repository.WishListStoreRepositoryImpl;
-import com.bbangle.bbangle.wishListStore.service.WishListStoreServiceImpl;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/*@AutoConfigureMockMvc*/
-@Import({TestSecurityConfig.class})
-@WebMvcTest(value = WishListStoreController.class,
-            excludeFilters = {
-                    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-                            classes = {WebOAuthSecurityConfig.class, CorsConfig.class})
-            })
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WishListStoreControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-
-    @MockBean
-    WishListStoreServiceImpl wishListStoreService;
-
-    @MockBean
-    MemberRepository memberRepository;
-
-    @MockBean
-    WishListFolderRepository wishListFolderRepository;
-
-    @MockBean
-    WishListFolderRepositoryImpl WishListFolderRepositoryImpl;
-
-    @MockBean
-    WishListStoreRepositoryImpl wishListStoreRepository;
 
     private static final Authentication AUTHENTICATION = new TestingAuthenticationToken(
             "test1@gmail.com",
             null,
             "ROLE_USER");
 
+    @Autowired
+    private WishListStoreController wishListStoreController;
+
+
     @BeforeEach
     public void setUpMockMvc() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new WishListStoreController(wishListStoreService)).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(wishListStoreController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .build();
     }
 
     private final String BEARER = "Bearer";
-    private final String AUTHORIZATION = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiYmFuZ2xlYmJhbmdsZSIsImlhdCI6MTcxMDU5NDgzMSwiZXhwIjoxNzEwNjA1NjMxLCJzdWIiOiJkc3lvb24xOTk0QGdtYWlsLmNvbSIsImlkIjoxMn0.EdRBBy5Orzlv_oZm4hGRZQZZ79_H-1JJHjzXZxlM_YU";
+    private final String AUTHORIZATION = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiYmFuZ2xlYmJhbmdsZSIsImlhdCI6MTcxMTgxNTQwNiwiZXhwIjoxNzExODI2MjA2LCJpZCI6MTB9._eIbWeiRajOKQpUPU0GjKJP7OmCBT3sdD9tWy9hjCr8";
 
     @DisplayName("위시리스트 스토어 전체 조회를 시행한다 오프셋 기반 페이지 네이션")
     @Test
@@ -85,9 +55,10 @@ public class WishListStoreControllerTest {
                 .param("size", "1")
                 .param("sort", "createdAt,DESC"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.contents[0].introduce").value("건강을 먹다_로썸"))
+                .andExpect(jsonPath("$.contents[0].introduce").value("건강을 먹다-로썸"))
                 .andDo(print());
     }
+
 
     @DisplayName("위시리스트 스토어 전체 조회를 시행한다 커서 기반 페이지 네이션")
     @Test
@@ -97,7 +68,7 @@ public class WishListStoreControllerTest {
                         .with(SecurityMockMvcRequestPostProcessors.authentication(AUTHENTICATION))
                         .param("size", "5"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.contents[0].introduce").value("건강을 먹다_로썸"))
+                .andExpect(jsonPath("$.contents[0].introduce").value("건강을 먹다-로썸"))
                 .andDo(print());
     }
 }
