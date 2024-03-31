d6918d32236819a7336dca9e03d8fd0299af8707
sikdong <zmstnv1119@naver.com>
2024-03-31 09:27:37 +0900

    feat : WebMvcTest 용 시 JWT 인증 무시 위한 설정 클래스 작성

--- src/test/java/com/bbangle/bbangle/configuration/TestSecurityConfig.java
+++ src/test/java/com/bbangle/bbangle/configuration/TestSecurityConfig.java
@@ -2,16 +2,30 @@ package com.bbangle.bbangle.configuration;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+/**
+ * WebMvcTest 일 때 사용
+ */
 @TestConfiguration
-public class TestSecurityConfig { // JWT 인증과정을 무시하기 위한 테스트용 시큐리티 config
+public class TestSecurityConfig implements WebMvcConfigurer { // JWT 인증과정을 무시하기 위한 테스트용 시큐리티 config
     @Bean
     public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         return http.build();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add( new PageableHandlerMethodArgumentResolver());
     }
 }
