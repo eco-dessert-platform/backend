3bbc6e53f8d87bfd742dfe11d91441f11d6eb0ed
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-05-29 05:19:42 +0900

    협업을 위한 임시데이터 생성 메서드 구현

--- src/main/java/com/bbangle/bbangle/board/dto/ProductResponse.java
+++ src/main/java/com/bbangle/bbangle/board/dto/ProductResponse.java
@@ -1,13 +1,16 @@
 package com.bbangle.bbangle.board.dto;
 
 import java.util.List;
+import java.util.Random;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
-public record ProductResponse(
-    Boolean boardIsBundled,
-    List<BoardDetailProductDto> products
-) {
+public class ProductResponse {
+
+    Boolean boardIsBundled;
+    List<BoardDetailProductDto> products;
 
     public static ProductResponse of(
         Boolean boardIsBundled,
@@ -17,5 +20,27 @@ public record ProductResponse(
             .boardIsBundled(boardIsBundled)
             .products(products)
             .build();
+    }
+
+    // 프론트 API 연동 테스트를 위한 값입니다. 해당 기능 구현 시 삭제됩니다.
+    public ProductResponse toFixture() {
+        Random random = new Random();
+
+        List<BoardDetailProductDto> boardDetailProductDtos = List.of(
+            BoardDetailProductDto.builder().build().toFixture(),
+            BoardDetailProductDto.builder().build().toFixture(),
+            BoardDetailProductDto.builder().build().toFixture());
+
+        if (random.nextBoolean()) {
+            return ProductResponse.builder()
+                .boardIsBundled(random.nextBoolean())
+                .products(boardDetailProductDtos)
+                .build();
+        } else {
+            return ProductResponse.builder()
+                .boardIsBundled(random.nextBoolean())
+                .products(boardDetailProductDtos)
+                .build();
+        }
     }
 }
