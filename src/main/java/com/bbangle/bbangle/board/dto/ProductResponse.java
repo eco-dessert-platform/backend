403c3863162e7c8f007da0e7cdd1d020dd8b014c
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-02 11:48:52 +0900

    가짜데이터 제거

--- src/main/java/com/bbangle/bbangle/board/dto/ProductResponse.java
+++ src/main/java/com/bbangle/bbangle/board/dto/ProductResponse.java
@@ -1,7 +1,6 @@
 package com.bbangle.bbangle.board.dto;
 
 import java.util.List;
-import java.util.Random;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,11 +9,11 @@ import lombok.Getter;
 public class ProductResponse {
 
     Boolean boardIsBundled;
-    List<BoardDetailProductDto> products;
+    List<ProductDto> products;
 
     public static ProductResponse of(
         Boolean boardIsBundled,
-        List<BoardDetailProductDto> products
+        List<ProductDto> products
     ) {
         return ProductResponse.builder()
             .boardIsBundled(boardIsBundled)
@@ -22,25 +21,4 @@ public class ProductResponse {
             .build();
     }
 
-    // 프론트 API 연동 테스트를 위한 값입니다. 해당 기능 구현 시 삭제됩니다.
-    public ProductResponse toFixture() {
-        Random random = new Random();
-
-        List<BoardDetailProductDto> boardDetailProductDtos = List.of(
-            BoardDetailProductDto.builder().build().toFixture(),
-            BoardDetailProductDto.builder().build().toFixture(),
-            BoardDetailProductDto.builder().build().toFixture());
-
-        if (random.nextBoolean()) {
-            return ProductResponse.builder()
-                .boardIsBundled(random.nextBoolean())
-                .products(boardDetailProductDtos)
-                .build();
-        } else {
-            return ProductResponse.builder()
-                .boardIsBundled(random.nextBoolean())
-                .products(boardDetailProductDtos)
-                .build();
-        }
-    }
 }
