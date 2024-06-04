b0903b0674b82564e0259968a6203b58002ae922
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-05 03:28:52 +0900

    fixtureMember, fixtureReview 추가

--- src/test/java/com/bbangle/bbangle/AbstractIntegrationTest.java
+++ src/test/java/com/bbangle/bbangle/AbstractIntegrationTest.java
@@ -10,13 +10,17 @@ import com.bbangle.bbangle.board.domain.ProductImg;
 import com.bbangle.bbangle.board.repository.BoardDetailRepository;
 import com.bbangle.bbangle.board.repository.BoardRepository;
 import com.bbangle.bbangle.board.repository.ProductRepository;
+import com.bbangle.bbangle.member.domain.Member;
 import com.bbangle.bbangle.member.repository.MemberRepository;
 import com.bbangle.bbangle.member.service.MemberService;
 import com.bbangle.bbangle.ranking.domain.Ranking;
 import com.bbangle.bbangle.ranking.repository.RankingRepository;
+import com.bbangle.bbangle.review.domain.Review;
+import com.bbangle.bbangle.review.repository.ReviewRepository;
 import com.bbangle.bbangle.store.domain.Store;
 import com.bbangle.bbangle.store.repository.StoreRepository;
 import com.bbangle.bbangle.store.service.StoreService;
+import com.bbangle.bbangle.wishlist.domain.WishListFolder;
 import com.bbangle.bbangle.wishlist.repository.WishListBoardRepository;
 import com.bbangle.bbangle.wishlist.repository.WishListFolderRepository;
 import com.bbangle.bbangle.wishlist.repository.WishListStoreRepository;
@@ -74,6 +78,8 @@ public abstract class AbstractIntegrationTest {
     protected BoardImgRepository boardImgRepository;
     @Autowired
     protected BoardDetailRepository boardDetailRepository;
+    @Autowired
+    protected ReviewRepository reviewRepository;
 
     @BeforeEach
     void before() {
@@ -88,6 +94,7 @@ public abstract class AbstractIntegrationTest {
         wishListFolderRepository.deleteAllInBatch();
         wishListBoardRepository.deleteAllInBatch();
         wishListStoreRepository.deleteAllInBatch();
+        reviewRepository.deleteAllInBatch();
     }
 
     protected FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
@@ -97,6 +104,27 @@ public abstract class AbstractIntegrationTest {
     /**
      * NOTE: param 에 변경하고자 하는 필드명 : 값 형식으로 주입하면 변경되어 insert 됨
      */
+
+    protected WishListFolder fixtureWishListFolder(Map<String, Object> params) {
+        ArbitraryBuilder<WishListFolder> builder = fixtureMonkey.giveMeBuilder(
+            WishListFolder.class);
+        setBuilderParams(params, builder);
+
+        if (!params.containsKey("member")) {
+            Member member = fixtureMember(emptyMap());
+            builder = builder.set("member", member); // store wishlist가 없으면 에러나서 추가
+        }
+
+        return wishListFolderRepository.save(builder.sample());
+    }
+
+    protected Member fixtureMember(Map<String, Object> params) {
+        ArbitraryBuilder<Member> builder = fixtureMonkey.giveMeBuilder(Member.class);
+        setBuilderParams(params, builder);
+
+        return memberRepository.save(builder.sample());
+    }
+
     protected Store fixtureStore(Map<String, Object> params) {
         ArbitraryBuilder<Store> builder = fixtureMonkey.giveMeBuilder(Store.class);
         setBuilderParams(params, builder);
@@ -181,6 +209,13 @@ public abstract class AbstractIntegrationTest {
         );
 
         return board;
+    }
+
+    protected Review fixtureReview(Map<String, Object> params) {
+        ArbitraryBuilder<Review> builder = fixtureMonkey.giveMeBuilder(Review.class);
+        setBuilderParams(params, builder);
+
+        return reviewRepository.save(builder.sample());
     }
 
     private void setBuilderParams(Map<String, Object> params, ArbitraryBuilder builder) {
