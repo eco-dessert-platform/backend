f60880d844b9ab7750fe67b170112466961bfdc1
sikdong <zmstnv1119@naver.com>
2024-06-05 00:59:54 +0900

    feat: 스프링 스케줄러 이용한 Best Review 업데이트 기능 구현

--- src/main/java/com/bbangle/bbangle/review/dto/ReviewSingleDto.java
+++ src/main/java/com/bbangle/bbangle/review/dto/ReviewSingleDto.java
@@ -1,20 +1,21 @@
 package com.bbangle.bbangle.review.dto;
 
-import com.bbangle.bbangle.common.domain.Badge;
+import com.bbangle.bbangle.review.domain.Badge;
 import com.querydsl.core.annotations.QueryProjection;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ReviewSingleDto(
-        Long id,
-        String nickname,
-        BigDecimal rate,
-        Badge badgeTaste,
-        Badge badgeBrix,
-        Badge badgeTexture,
-        String content,
-        LocalDateTime createdAt
+    Long id,
+    String nickname,
+    BigDecimal rate,
+    Badge badgeTaste,
+    Badge badgeBrix,
+    Badge badgeTexture,
+    String content,
+    LocalDateTime createdAt,
+    Boolean isBest
 ) {
     @QueryProjection
     public ReviewSingleDto { // Noncompliant - method is empty
