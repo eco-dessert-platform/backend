ae9b493e870fa5959b15e7b55b116ab1961d7104
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-05 03:32:00 +0900

    테스트 코드 작성

--- src/test/java/com/bbangle/bbangle/review/controller/ReviewControllerTest.java
+++ src/test/java/com/bbangle/bbangle/review/controller/ReviewControllerTest.java
@@ -2,17 +2,16 @@ package com.bbangle.bbangle.review.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.bbangle.bbangle.AbstractIntegrationTest;
-import com.bbangle.bbangle.review.service.ReviewService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 class ReviewControllerTest extends AbstractIntegrationTest {
+
     @Autowired
     MockMvc mockMvc;
 
