ae860746a87ab0a910b704566fa43252c3a81649
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-05 02:24:18 +0900

    서비스 로직 구현

--- src/main/java/com/bbangle/bbangle/review/service/ReviewService.java
+++ src/main/java/com/bbangle/bbangle/review/service/ReviewService.java
@@ -1,16 +1,16 @@
 package com.bbangle.bbangle.review.service;
 
-import static com.bbangle.bbangle.exception.BbangleErrorCode.NOTFOUND_MEMBER;
-
 import com.bbangle.bbangle.common.domain.Badge;
 import com.bbangle.bbangle.common.image.service.S3Service;
-import com.bbangle.bbangle.exception.BbangleException;
 import com.bbangle.bbangle.member.repository.MemberRepository;
 import com.bbangle.bbangle.review.domain.Review;
 import com.bbangle.bbangle.review.domain.ReviewImg;
+import com.bbangle.bbangle.review.dto.ReviewDto;
 import com.bbangle.bbangle.review.dto.ReviewRequest;
+import com.bbangle.bbangle.review.dto.SummarizedReviewResponse;
 import com.bbangle.bbangle.review.repository.ReviewImgRepository;
 import com.bbangle.bbangle.review.repository.ReviewRepository;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +21,14 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
+
     private final ReviewRepository reviewRepository;
     private final ReviewImgRepository reviewImgRepository;
     private final MemberRepository memberRepository;
     private final S3Service s3Service;
+    private final ReviewStatistics reviewStatistics;
     private static final String BOARD_FOLDER = "board/";
-    private static final String REVIEW_FOLDER= "/review/";
+    private static final String REVIEW_FOLDER = "/review/";
 
     @Transactional
     public void makeReview(ReviewRequest reviewRequest, Long memberId) {
@@ -40,7 +42,7 @@ public class ReviewService {
         List<Badge> badges = reviewRequest.badges();
         badges.forEach(review::insertBadge);
         reviewRepository.save(review);
-        if(Objects.isNull(reviewRequest.photos())){
+        if (Objects.isNull(reviewRequest.photos())) {
             return;
         }
         makeReviewImg(reviewRequest, review);
@@ -51,10 +53,25 @@ public class ReviewService {
         Long boardId = reviewRequest.boardId();
         List<MultipartFile> photos = reviewRequest.photos();
         photos.stream()
-            .map(photo -> s3Service.saveImage(photo, BOARD_FOLDER + boardId + REVIEW_FOLDER + reviewId))
+            .map(photo -> s3Service.saveImage(photo,
+                BOARD_FOLDER + boardId + REVIEW_FOLDER + reviewId))
             .forEach(reviewImgPath -> reviewImgRepository.save(ReviewImg.builder()
-                                        .reviewId(reviewId)
-                                        .url(reviewImgPath)
-                                        .build()));
+                .reviewId(reviewId)
+                .url(reviewImgPath)
+                .build()));
+    }
+
+    public SummarizedReviewResponse getSummarizedReview(Long boardId) {
+        List<ReviewDto> reviews = reviewRepository.findByBoardId(boardId);
+
+        if (reviews.isEmpty()) {
+            return SummarizedReviewResponse.getEmpty();
+        }
+
+        BigDecimal averageRatingScore = reviewStatistics.getAverageRatingScore(reviews);
+        int reviewCount = reviewStatistics.count(reviews);
+        List<String> popularBadgeList = reviewStatistics.getPopularBadgeList(reviews);
+
+        return SummarizedReviewResponse.of(averageRatingScore, reviewCount, popularBadgeList);
     }
 }
