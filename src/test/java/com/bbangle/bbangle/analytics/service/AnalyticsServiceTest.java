5cec5bb7e658ad5fd2806b4cb8e34c0a7202aabb
jungdonguk <foodsmith96@naver.com>
2024-06-07 12:37:37 +0900

    feat: PR 피드백 수정

--- src/test/java/com/bbangle/bbangle/analytics/service/AnalyticsServiceTest.java
+++ src/test/java/com/bbangle/bbangle/analytics/service/AnalyticsServiceTest.java
@@ -1,7 +1,8 @@
 package com.bbangle.bbangle.analytics.service;
 
 import com.bbangle.bbangle.AbstractIntegrationTest;
-import com.bbangle.bbangle.analytics.dto.*;
+import com.bbangle.bbangle.analytics.dto.AnalyticsCountWithDateResponseDto;
+import com.bbangle.bbangle.analytics.dto.AnalyticsRatioWithDateResponseDto;
 import com.bbangle.bbangle.board.domain.Board;
 import com.bbangle.bbangle.common.domain.Badge;
 import com.bbangle.bbangle.member.domain.Member;
@@ -24,6 +25,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,15 +36,31 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
 
 
     @Test
-    @DisplayName("신규 회원의 수가 정상적으로 조회된다.")
+    @DisplayName("전체 회원의 수가 정상적으로 조회된다.")
+    void countAllMember() {
+        // given
+        LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
+        create10DaysAgoMembers(createdAt);
+        createMembers();
+
+        // when
+        long result = analyticsService.countAllMembers().count();
+
+        // then
+        assertThat(result).isEqualTo(20);
+    }
+
+
+    @Test
+    @DisplayName("기간 내 가입한 회원의 수가 정상적으로 조회된다.")
     void countNewMember() {
         // given
         LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
         create10DaysAgoMembers(createdAt);
         createMembers();
 
-        LocalDate startDate = LocalDate.now().minusDays(9);
-        LocalDate endDate = LocalDate.now();
+        Optional<LocalDate> startDate = Optional.of(LocalDate.now().minusDays(9));
+        Optional<LocalDate> endDate = Optional.of(LocalDate.now());
 
         // then
         long membersCount = memberRepository.count();
@@ -55,23 +73,7 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
 
 
     @Test
-    @DisplayName("전체 회원의 수가 정상적으로 조회된다.")
-    void countAllMember() {
-        // given
-        LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
-        create10DaysAgoMembers(createdAt);
-        createMembers();
-
-        // when
-        long result = analyticsService.countAllMembers();
-
-        // then
-        assertThat(result).isEqualTo(20);
-    }
-
-
-    @Test
-    @DisplayName("기간 별 회원 대비 위시리스트 이용 비율이 성공적으로 조회된다.")
+    @DisplayName("기간 내 일별 위시리스트 이용 비율이 성공적으로 조회된다.")
     void countMembersUsingWishlist() {
         // given
         LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
@@ -79,15 +81,17 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
         createMembers();
         createMembers();
         createWishListBoards(members1);
-        create10DaysAgoWishlistBoards(createdAt);
+        create10DaysAgoWishlistBoards(members1, createdAt);
 
+        Optional<LocalDate> optStartDate = Optional.of(LocalDate.now().minusDays(10));
+        Optional<LocalDate> optEndDate = Optional.of(LocalDate.now());
         LocalDate startDate = LocalDate.now().minusDays(10);
         LocalDate endDate = LocalDate.now();
 
         // when
         long membersCount = memberRepository.count();
         List<AnalyticsCountWithDateResponseDto> countWithDateResponseDtos = wishListBoardRepository.countMembersUsingWishlistBetweenPeriod(startDate, endDate);
-        List<AnalyticsRatioWithDateResponseDto> results = analyticsService.calculateWishlistUsingRatio(startDate, endDate);
+        List<AnalyticsRatioWithDateResponseDto> results = analyticsService.calculateWishlistUsingRatio(optStartDate, optEndDate);
 
         // then
         assertThat(membersCount).isEqualTo(30);
@@ -101,18 +105,18 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
 
 
     @Test
-    @DisplayName("기간 별 위시리스트 총 개수가 정상적으로 조회된다.")
+    @DisplayName("기간 별 위시리스트 누적 개수가 정상적으로 조회된다.")
     void countWishlistBoardByPeriod() {
         // given
         List<Member> members = createMembers();
         createWishListBoards(members);
 
         LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
-        create10DaysAgoWishlistBoards(createdAt);
+        create10DaysAgoWishlistBoards(members, createdAt);
 
         // when
-        LocalDate startDate = LocalDate.now().minusDays(9);
-        LocalDate endDate = LocalDate.now();
+        Optional<LocalDate> startDate = Optional.of(LocalDate.now().minusDays(9));
+        Optional<LocalDate> endDate = Optional.of(LocalDate.now());
         Long wishlistBoardsCount = wishListBoardRepository.count();
         List<AnalyticsCountWithDateResponseDto> results = analyticsService.countWishlistBoardByPeriod(startDate, endDate);
 
@@ -126,7 +130,7 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
 
 
     @Test
-    @DisplayName("기간 별 회원 대비 리뷰 이용 비율이 정상적으로 조회된다.")
+    @DisplayName("기간 내 일별 리뷰 이용 비율이 정상적으로 조회된다.")
     void calculateReviewUsingRatio() {
         // given
         LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
@@ -137,13 +141,15 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
         createReviews(members1);
         create10DaysAgoReviews(members1, createdAt);
 
+        Optional<LocalDate> optStartDate = Optional.of(LocalDate.now().minusDays(10));
+        Optional<LocalDate> optEndDate = Optional.of(LocalDate.now());
         LocalDate startDate = LocalDate.now().minusDays(10);
         LocalDate endDate = LocalDate.now();
 
         // when
         long membersCount = memberRepository.count();
         List<AnalyticsCountWithDateResponseDto> analyticsCountWithDateResponseDtos = reviewRepository.countMembersUsingReviewBetweenPeriod(startDate, endDate);
-        List<AnalyticsRatioWithDateResponseDto> results = analyticsService.calculateReviewUsingRatio(startDate, endDate);
+        List<AnalyticsRatioWithDateResponseDto> results = analyticsService.calculateReviewUsingRatio(optStartDate, optEndDate);
 
         // then
         assertThat(membersCount).isEqualTo(40);
@@ -156,7 +162,7 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
 
 
     @Test
-    @DisplayName("기간 별 리뷰 총 생성 개수가 정상적으로 조회된다.")
+    @DisplayName("기간 별 리뷰 누적 개수가 정상적으로 조회된다.")
     void countReviewByPeriod() {
         // given
         LocalDateTime createdAt = LocalDateTime.now().minusDays(10);
@@ -260,28 +266,7 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
     }
 
 
-    private void createBoards(List<Member> members) {
-        for (int i = 1; i <= members.size(); i++) {
-            Store store = Store.builder()
-                    .id((long) i + 1)
-                    .name("test" + i)
-                    .introduce("introduce" + i)
-                    .isDeleted(false)
-                    .build();
-            storeRepository.save(store);
-
-            Board board = Board.builder()
-                    .id((long) i + 1)
-                    .store(store)
-                    .title("title" + i)
-                    .wishCnt(i)
-                    .build();
-            boardRepository.save(board);
-        }
-    }
-
-
-    private void create10DaysAgoWishlistBoards(LocalDateTime createdAt) {
+    private void create10DaysAgoWishlistBoards(List<Member> members, LocalDateTime createdAt) {
         TransactionStatus status = null;
 
         try {
@@ -289,8 +274,9 @@ class AnalyticsServiceTest extends AbstractIntegrationTest {
             transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
             status = tm.getTransaction(transactionDefinition);
 
-            for (int i = 1; i <= 10; i++) {
+            for (int i = 0; i < 10; i++) {
                 WishListBoard wishListBoard = WishListBoard.builder()
+                        .memberId(members.get(i).getId())
                         .build();
 
                 wishListBoardRepository.save(wishListBoard);
