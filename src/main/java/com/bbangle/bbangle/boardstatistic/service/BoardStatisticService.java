869081463832d37196fd5595ba782fd555a8c4c2
shoeone96 <dlwnddnjs96@gmail.com>
2024-06-10 17:06:57 +0900

    board 통계 관련 로직 작성

--- src/main/java/com/bbangle/bbangle/boardstatistic/service/BoardStatisticService.java
+++ src/main/java/com/bbangle/bbangle/boardstatistic/service/BoardStatisticService.java
@@ -9,12 +9,17 @@ import com.bbangle.bbangle.boardstatistic.repository.BoardStatisticRepository;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardStatisticService {
+
+    private static final Double BOARD_WISH_SCORE = 50.0;
+    private static final Double BOARD_VIEW_SCORE = 1.0;
+    private static final Double BOARD_REVIEW_SCORE = 50.0;
 
     private final BoardStatisticRepository boardStatisticRepository;
     private final BoardRepository boardRepository;
@@ -35,12 +40,35 @@ public class BoardStatisticService {
         boardStatisticRepository.saveAll(boardStatistics);
     }
 
+    @Async
     @Transactional
-    public void updateRankingScore(Long boardId, Double updatingScore) {
+    public void updateViewCount(Long boardId) {
         BoardStatistic boardStatistic = boardStatisticRepository.findByBoardId(boardId)
             .orElseThrow(
                 () -> new BbangleException(BbangleErrorCode.RANKING_NOT_FOUND));
-        boardStatistic.updateBasicScore(updatingScore);
+        boardStatistic.updateViewCount();
+        boardStatistic.updateBasicScore(BOARD_VIEW_SCORE, true);
+    }
+
+    @Async
+    @Transactional
+    public void updateWishCount(Long boardId, boolean isWish) {
+        BoardStatistic boardStatistic = boardStatisticRepository.findByBoardId(boardId)
+            .orElseThrow(
+                () -> new BbangleException(BbangleErrorCode.RANKING_NOT_FOUND));
+
+        boardStatistic.updateWishCount(isWish);
+        boardStatistic.updateBasicScore(BOARD_WISH_SCORE, isWish);
+    }
+
+    @Async
+    @Transactional
+    public void updateReviewCount(Long boardId, boolean isCreate) {
+        BoardStatistic boardStatistic = boardStatisticRepository.findByBoardId(boardId)
+            .orElseThrow(
+                () -> new BbangleException(BbangleErrorCode.RANKING_NOT_FOUND));
+        boardStatistic.updateReviewCount(isCreate);
+        boardStatistic.updateBasicScore(BOARD_REVIEW_SCORE, isCreate);
     }
 
 }
