21ad9fa7d38ad792eb15f1913f4ef9857561a5a0
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-12 08:40:27 +0900

    사용안하는 import문 삭제

--- src/test/java/com/bbangle/bbangle/store/service/StoreServiceTest.java
+++ src/test/java/com/bbangle/bbangle/store/service/StoreServiceTest.java
@@ -26,7 +26,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 class StoreServiceTest extends AbstractIntegrationTest {
 
