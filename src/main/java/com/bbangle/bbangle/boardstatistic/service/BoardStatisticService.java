2dbc51fb1fb894b39b4814cbf2089ebdbdf1e51f
shoeone96 <dlwnddnjs96@gmail.com>
2024-06-12 01:15:31 +0900

    서버를 띄울 때 boardStatistic 정보를 업데이트 해주는 서비스 로직 추가

--- src/main/java/com/bbangle/bbangle/boardstatistic/service/BoardStatisticService.java
+++ src/main/java/com/bbangle/bbangle/boardstatistic/service/BoardStatisticService.java
@@ -2,11 +2,16 @@ package com.bbangle.bbangle.boardstatistic.service;
 
 import com.bbangle.bbangle.board.domain.Board;
 import com.bbangle.bbangle.board.repository.BoardRepository;
+import com.bbangle.bbangle.config.ranking.BoardGrade;
+import com.bbangle.bbangle.config.ranking.BoardWishCount;
 import com.bbangle.bbangle.exception.BbangleErrorCode;
 import com.bbangle.bbangle.exception.BbangleException;
 import com.bbangle.bbangle.boardstatistic.domain.BoardStatistic;
 import com.bbangle.bbangle.boardstatistic.repository.BoardStatisticRepository;
-import jakarta.validation.constraints.NotNull;
+import com.bbangle.bbangle.review.repository.ReviewRepository;
+import com.bbangle.bbangle.review.service.ReviewService;
+import com.bbangle.bbangle.wishlist.repository.WishListBoardRepository;
+import com.bbangle.bbangle.wishlist.service.WishListBoardService;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +30,8 @@ public class BoardStatisticService {
 
     private final BoardStatisticRepository boardStatisticRepository;
     private final BoardRepository boardRepository;
+    private final ReviewRepository reviewRepository;
+    private final WishListBoardRepository wishListBoardRepository;
 
     public void updatingNonRankedBoards() {
         List<Board> unRankedBoards = boardRepository.checkingNullRanking();
@@ -40,6 +47,45 @@ public class BoardStatisticService {
                 .build())
             .forEach(boardStatistics::add);
         boardStatisticRepository.saveAll(boardStatistics);
+    }
+
+    @Transactional
+    public void checkingStatisticStatusAndUpdate() {
+        List<BoardStatistic> allStatistics = boardStatisticRepository.findAll();
+        List<BoardWishCount> wishCountList = wishListBoardRepository.groupByBoardIdAndGetWishCount();
+        List<BoardGrade> reviewGradeList = reviewRepository.groupByBoardIdAndGetReviewCountAndReviewRate();
+        updatingBoardWishCount(wishCountList, allStatistics);
+        updatingBoardReviewCountAndRate(reviewGradeList, allStatistics);
+        for(BoardStatistic statistic : allStatistics){
+            statistic.updateBasicScoreWhenInit();
+        }
+    }
+
+    private void updatingBoardReviewCountAndRate(
+        List<BoardGrade> reviewGradeList,
+        List<BoardStatistic> allStatistics
+    ) {
+        for (BoardGrade boardGrade : reviewGradeList) {
+            for (BoardStatistic statistic : allStatistics) {
+                if (boardGrade.boardId().equals(statistic.getBoardId())) {
+                    statistic.setBoardReviewCountWhenInit(boardGrade.count());
+                    statistic.setBoardReviewRateWhenInit(boardGrade.grade());
+                }
+            }
+        }
+    }
+
+    private static void updatingBoardWishCount(
+        List<BoardWishCount> wishCountList,
+        List<BoardStatistic> allStatistics
+    ) {
+        for (BoardWishCount wishCount : wishCountList) {
+            for (BoardStatistic statistic : allStatistics) {
+                if (wishCount.boardId().equals(statistic.getBoardId())) {
+                    statistic.setBoardWishCountWhenInit(wishCount.count());
+                }
+            }
+        }
     }
 
     @Async
