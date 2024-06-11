492635cf168ea2c179a8dc174416806c96c87051
shoeone96 <dlwnddnjs96@gmail.com>
2024-06-12 01:17:32 +0900

    리뷰 데이터 작성을 위한 Fixture method 추가

--- src/test/java/com/bbangle/bbangle/fixture/ReviewFixture.java
+++ src/test/java/com/bbangle/bbangle/fixture/ReviewFixture.java
@@ -9,6 +9,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReviewFixture {
 
+    public static Review createReviewWithBoardId(Long boardId) {
+        return Review.builder()
+            .boardId(boardId)
+            .badgeBrix(Badge.BAD)
+            .content("content")
+            .rate(BigDecimal.valueOf(4.00))
+            .build();
+    }
+
+
+    public static Review createReviewWithBoardIdAndRate(Long boardId, double rate) {
+        return Review.builder()
+            .boardId(boardId)
+            .memberId(1L)
+            .badgeBrix(Badge.DRY)
+            .badgeTaste(Badge.GOOD)
+            .badgeTexture(Badge.SOFT)
+            .content("content")
+            .rate(BigDecimal.valueOf(rate))
+            .build();
+    }
+
     public static Review createReview() {
         return Review.builder()
             .badgeBrix(Badge.BAD)
