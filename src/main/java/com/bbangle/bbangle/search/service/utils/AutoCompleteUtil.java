25af27efb266026c7d1dedf030b6aa2d3925ffd2
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-26 14:44:40 +0900

    파일명 변경

---
+++ src/main/java/com/bbangle/bbangle/search/service/utils/AutoCompleteUtil.java
@@ -0,0 +1,82 @@
+package com.bbangle.bbangle.search.service.utils;
+
+import com.bbangle.bbangle.util.CharacterNode;
+import com.bbangle.bbangle.util.mQueue;
+import java.util.LinkedList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AutoCompleteUtil {
+
+    private final CharacterNode root = new CharacterNode("");
+
+    public void insert(String string) {
+        CharacterNode currentNode = this.root;
+
+        for (char character : string.toCharArray()) {
+            if (!currentNode.getChildren().containsKey(character)) {
+                currentNode.getChildren()
+                    .put(character, new CharacterNode(currentNode.getValue() + character));
+            }
+
+            currentNode = currentNode.getChildren().get(character);
+        }
+
+        currentNode.setWordExists(true);
+    }
+
+    public void insertAll(List<String> titles) {
+        for (String title : titles) {
+            insert(title);
+        }
+    }
+
+    public CharacterNode find(String string) {
+        CharacterNode currentNode = this.root;
+
+        for (char character : string.toCharArray()) {
+            if (!currentNode.getChildren().containsKey(character)) {
+                return null;
+            }
+            currentNode = currentNode.getChildren().get(character);
+        }
+
+        return currentNode;
+    }
+
+    public List<String> autoComplete(String prefix, int limit) {
+        CharacterNode prefixNode = find(prefix);
+        if (prefixNode == null || prefixNode.getChildren().isEmpty()) {
+            return List.of();
+        }
+
+        mQueue queue = new mQueue();
+        queue.enqueue(prefixNode);
+
+        LinkedList<String> words = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            CharacterNode currentNode = (CharacterNode) queue.dequeue();
+            if (currentNode.isWordExists()) {
+                limit--;
+
+                words.add(currentNode.getValue());
+
+                if (limit == 0) {
+                    return words;
+                }
+            }
+
+            for (CharacterNode child : currentNode.getChildren()
+                .values()) {
+                queue.enqueue(child);
+            }
+        }
+
+        return words;
+    }
+
+}
