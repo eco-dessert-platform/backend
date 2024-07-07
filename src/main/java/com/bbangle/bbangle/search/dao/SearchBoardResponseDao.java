b0de809fde55a23fbda92f1d402ee30a6f7d8162
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-07-08 02:47:14 +0900

    DAO 파일 생성

---
+++ src/main/java/com/bbangle/bbangle/search/dao/SearchBoardResponseDao.java
@@ -0,0 +1,37 @@
+package com.bbangle.bbangle.search.dao;
+
+import com.bbangle.bbangle.board.dao.TagsDao;
+import com.bbangle.bbangle.board.domain.Category;
+import com.querydsl.core.annotations.QueryProjection;
+
+public record SearchBoardResponseDao(
+    Long boardId,
+    Long storeId,
+    String storeName,
+    String thumbnail,
+    String title,
+    Integer price,
+    Category category,
+    TagsDao tagsDao
+) {
+
+    @QueryProjection
+    public SearchBoardResponseDao(
+        Long boardId,
+        Long storeId,
+        String storeName,
+        String thumbnail,
+        String title,
+        Integer price,
+        Category category,
+        boolean glutenFreeTag,
+        boolean highProteinTag,
+        boolean sugarFreeTag,
+        boolean veganTag,
+        boolean ketogenicTag
+    ) {
+        this(boardId, storeId, storeName, thumbnail, title, price, category,
+            new TagsDao(glutenFreeTag, highProteinTag, sugarFreeTag, veganTag, ketogenicTag));
+    }
+
+}
