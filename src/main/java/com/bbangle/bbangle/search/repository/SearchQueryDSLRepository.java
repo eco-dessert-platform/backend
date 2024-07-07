1b8f40915b3d3fc453325858c994823d763167a1
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-07-08 02:46:10 +0900

    필터 조건 추가

--- src/main/java/com/bbangle/bbangle/search/repository/SearchQueryDSLRepository.java
+++ src/main/java/com/bbangle/bbangle/search/repository/SearchQueryDSLRepository.java
@@ -1,19 +1,27 @@
 package com.bbangle.bbangle.search.repository;
 
+import com.bbangle.bbangle.board.dao.BoardResponseDao;
+import com.bbangle.bbangle.board.dto.FilterRequest;
+import com.bbangle.bbangle.board.sort.SortType;
 import com.bbangle.bbangle.search.dto.KeywordDto;
-import com.bbangle.bbangle.search.dto.request.SearchBoardRequest;
-import com.bbangle.bbangle.search.dto.response.SearchBoardResponse;
-import com.bbangle.bbangle.store.dto.StoreResponseDto;
 import com.bbangle.bbangle.member.domain.Member;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface SearchQueryDSLRepository {
-    Long getSearchedBoardAllCount(SearchBoardRequest boardRequest, List<Long> boardIds);
 
-    SearchBoardResponse getSearchedBoard(Long memberId, List<Long> boardIds, SearchBoardRequest searchBoardRequest, Pageable pageable, Long searchedBoardAllCount);
-    List<StoreResponseDto> getSearchedStore(Long memberId, List<Long> storeIndexList, Pageable pageable);
+    List<BoardResponseDao> getBoardResponseList(
+        List<Long> boardIds,
+        FilterRequest filterRequest,
+        SortType sort,
+        Long cursorId
+    );
+
+    Long getAllCount(
+        List<Long> boardIds,
+        FilterRequest filterRequest,
+        SortType sort
+    );
 
     List<KeywordDto> getRecencyKeyword(Member member);
 
