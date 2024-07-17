c93f25c059e49dba8f04afa17383663037a6b423
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-07-17 12:57:24 +0900

    equals, hashCode 메서드 추가

--- src/main/java/com/bbangle/bbangle/search/dto/KeywordDto.java
+++ src/main/java/com/bbangle/bbangle/search/dto/KeywordDto.java
@@ -1,14 +1,35 @@
 package com.bbangle.bbangle.search.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import java.util.Objects;
 
 public class KeywordDto {
 
-    String keyword;
+    private String keyword;
 
     @QueryProjection
     public KeywordDto(String keyword) {
         this.keyword = keyword;
     }
 
+    public String getKeyword() {
+        return keyword;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        KeywordDto that = (KeywordDto) obj;
+        return Objects.equals(keyword, that.keyword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(keyword);
+    }
 }
