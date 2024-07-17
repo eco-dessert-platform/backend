62bf93dc1bac4315bbfd9a363de4df675d2a4b83
shoeone96 <dlwnddnjs96@gmail.com>
2024-07-17 15:56:19 +0900

    동의를 확인하는 jpql 쿼리 추가(현재 memberId가 아닌 member 연관관계 매핑이 되어 있어 별도 메서드 작성)

--- src/main/java/com/bbangle/bbangle/member/repository/SignatureAgreementRepository.java
+++ src/main/java/com/bbangle/bbangle/member/repository/SignatureAgreementRepository.java
@@ -1,8 +1,12 @@
 package com.bbangle.bbangle.member.repository;
 
 import com.bbangle.bbangle.member.domain.SignatureAgreement;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SignatureAgreementRepository extends JpaRepository<SignatureAgreement, Long> {
 
+    @Query("select count(s.id) > 0 from SignatureAgreement s where s.member.id = :memberId")
+    boolean existsByMemberId(Long memberId);
 }
