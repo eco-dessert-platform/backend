803b0efc529ddbcf94da4df72f5bcf752461a9fe
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-10 17:25:13 +0900

    기본 취향반영 게시글 조회 Repository 추가

---
+++ src/main/java/com/bbangle/bbangle/boardstatistic/repository/BoardPreferenceStatisticRepository.java
@@ -0,0 +1,8 @@
+package com.bbangle.bbangle.boardstatistic.repository;
+
+import com.bbangle.bbangle.boardstatistic.domain.BoardPreferenceStatistic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardPreferenceStatisticRepository extends JpaRepository<BoardPreferenceStatistic, Long> {
+
+}
