379a149815a7addb8b391a9d928cab90bf4a6135
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-08-10 23:01:09 +0900

    QueryDSL로 구성이 안돼 직접 SQL문 작성

--- src/main/java/com/bbangle/bbangle/board/repository/BoardRepository.java
+++ src/main/java/com/bbangle/bbangle/board/repository/BoardRepository.java
@@ -8,20 +8,67 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardQueryDSLRepository {
-
-    /*
-    * 실행 순서
-    * 1. Product, Board INNER JOIN
-    * 2. WishListBoard LEFT JOIN
-    * 3. boardIds로 WHERE 조건절 수행
-    * 4. GROUP BY boardId로 집계
-    * 5. ORDER BY로 정렬
-    * */
     @Query("SELECT new com.bbangle.bbangle.board.dto.BoardInfoDto(" +
         "b.id, " +
         "MIN(b.profile), " +
         "MIN(b.title), " +
         "MIN(b.price), " +
+        "MIN(b.discountRate), " +
+        "MIN(s.boardReviewGrade), " +
+        "MIN(s.boardReviewCount), " +
+        "MAX(CASE WHEN p.soldout = false THEN 0 ELSE 1 END), " +
+        "CASE WHEN COUNT(p.orderStartDate) > 0 THEN true ELSE false END, " +
+        "MAX(CASE WHEN p.glutenFreeTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.highProteinTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.sugarFreeTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.veganTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.ketogenicTag = true THEN 1 ELSE 0 END), " +
+        "CASE WHEN COUNT(DISTINCT p.category) > 1 THEN true ELSE false END, " +
+        "CASE WHEN w.wishlistFolderId IS NOT NULL THEN true ELSE false END) " +
+        "FROM Product p " +
+        "JOIN Board b ON b.store.id = :storeId AND p.board.id = b.id " +
+        "JOIN BoardStatistic s ON s.boardId = b.id " +
+        "LEFT JOIN WishListBoard w ON b.id = w.boardId AND w.memberId = :memberId " +
+        "GROUP BY b.id " +
+        "ORDER BY b.id desc " +
+        "LIMIT 11")
+    List<BoardInfoDto> findBoardsByStore(@Param("storeId") Long storeId, @Param("memberId") Long memberId);
+
+    @Query("SELECT new com.bbangle.bbangle.board.dto.BoardInfoDto(" +
+        "b.id, " +
+        "MIN(b.profile), " +
+        "MIN(b.title), " +
+        "MIN(b.price), " +
+        "MIN(b.discountRate), " +
+        "MIN(s.boardReviewGrade), " +
+        "MIN(s.boardReviewCount), " +
+        "MAX(CASE WHEN p.soldout = false THEN 0 ELSE 1 END), " +
+        "CASE WHEN COUNT(p.orderStartDate) > 0 THEN true ELSE false END, " +
+        "MAX(CASE WHEN p.glutenFreeTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.highProteinTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.sugarFreeTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.veganTag = true THEN 1 ELSE 0 END), " +
+        "MAX(CASE WHEN p.ketogenicTag = true THEN 1 ELSE 0 END), " +
+        "CASE WHEN COUNT(DISTINCT p.category) > 1 THEN true ELSE false END, " +
+        "CASE WHEN w.wishlistFolderId IS NOT NULL THEN true ELSE false END) " +
+        "FROM Product p " +
+        "JOIN Board b ON b.store.id = :storeId AND p.board.id = b.id " +
+        "JOIN BoardStatistic s ON s.boardId = b.id " +
+        "LEFT JOIN WishListBoard w ON b.id = w.boardId AND w.memberId = :memberId " +
+        "WHERE b.id <= :cursorId " +
+        "GROUP BY b.id " +
+        "ORDER BY b.id desc " +
+        "LIMIT 11")
+    List<BoardInfoDto> findBoardsByStoreWithCursor(@Param("storeId") Long storeId, @Param("memberId") Long memberId, @Param("cursorId") Long cursorId);
+
+    @Query("SELECT new com.bbangle.bbangle.board.dto.BoardInfoDto(" +
+        "b.id, " +
+        "MIN(b.profile), " +
+        "MIN(b.title), " +
+        "MIN(b.price), " +
+        "MIN(b.discountRate), " +
+        "MIN(s.boardReviewGrade), " +
+        "MIN(s.boardReviewCount), " +
         "MAX(CASE WHEN p.soldout = false THEN 0 ELSE 1 END), " +
         "CASE WHEN COUNT(p.orderStartDate) > 0 THEN true ELSE false END, " +
         "MAX(CASE WHEN p.glutenFreeTag = true THEN 1 ELSE 0 END), " +
@@ -33,9 +80,11 @@ public interface BoardRepository extends JpaRepository<Board, Long>, BoardQueryD
         "CASE WHEN w.wishlistFolderId IS NOT NULL THEN true ELSE false END) " +
         "FROM Product p " +
         "JOIN Board b ON p.board.id = b.id " +
+        "JOIN BoardStatistic s ON s.boardId = b.id " +
         "LEFT JOIN WishListBoard w ON b.id = w.boardId AND w.memberId = :memberId " +
-        "WHERE b.id IN :boardIds " +
-        "GROUP BY b.id " +
-        "ORDER BY b.id desc ")
-    List<BoardInfoDto> findTagCategoriesByBoardIds(@Param("boardIds") List<Long> boardIds, @Param("memberId") Long memberId);
+        "WHERE b.store.id = :storeId " +
+        "GROUP BY s.basicScore, b.id " +
+        "ORDER BY s.basicScore desc " +
+        "LIMIT 3")
+    List<BoardInfoDto> findBestBoards(@Param("memberId") Long memberId, @Param("storeId") Long storeId);
 }
