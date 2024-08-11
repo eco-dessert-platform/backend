60b48f51d3614f28e971a6ef7a91e2626e8ad14e
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-11 16:08:03 +0900

    @Nested 기반 테스트로 변경

--- src/test/java/com/bbangle/bbangle/boardstatistic/service/BoardPreferenceStatisticServiceTest.java
+++ src/test/java/com/bbangle/bbangle/boardstatistic/service/BoardPreferenceStatisticServiceTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -34,38 +35,47 @@ class BoardPreferenceStatisticServiceTest extends AbstractIntegrationTest {
     private Product product3;
     private List<Board> boardList = new ArrayList<>();
 
-    @BeforeEach
-    void setup(){
-        Store store = StoreFixture.storeGenerator();
-        store = storeRepository.save(store);
-        board1 = BoardFixture.randomBoard(store);
-        board2 = BoardFixture.randomBoard(store);
-        board1 = boardRepository.save(board1);
-        board2 = boardRepository.save(board2);
-        boardList.add(board1);
-        boardList.add(board2);
+    @Nested
+    @DisplayName("게시글에 매칭되는 boardId가 개인 선호 통계 테이블에 없을 때 업데이트")
+    class updatingNotGeneratedBoardPreference {
 
-        product1 = ProductFixture.randomProduct(board1);
-        product2 = ProductFixture.randomProduct(board1);
-        product3 = ProductFixture.randomProduct(board2);
+        @BeforeEach
+        void setup() {
+            Store store = StoreFixture.storeGenerator();
+            store = storeRepository.save(store);
+            board1 = BoardFixture.randomBoard(store);
+            board2 = BoardFixture.randomBoard(store);
+            board1 = boardRepository.save(board1);
+            board2 = boardRepository.save(board2);
+            boardList.add(board1);
+            boardList.add(board2);
 
-        product1 = productRepository.save(product1);
-        product2 = productRepository.save(product2);
-        product3 = productRepository.save(product3);
+            product1 = ProductFixture.randomProduct(board1);
+            product2 = ProductFixture.randomProduct(board1);
+            product3 = ProductFixture.randomProduct(board2);
+
+            product1 = productRepository.save(product1);
+            product2 = productRepository.save(product2);
+            product3 = productRepository.save(product3);
+        }
+
+        @Test
+        @DisplayName("정상적으로 존재하지 않는 선호기반 게시글 통계자료를 생성한다")
+        void generateBoardPreferenceStatisticList() {
+            //given
+            preferenceStatisticService.updatingNonRankedBoards();
+
+            //when
+            List<BoardWithTagDao> boardWithTagDaos = boardRepository.checkingNullWithPreferenceRanking();
+            List<BoardPreferenceStatistic> allPreferenceStatisticList = preferenceStatisticRepository.findAll();
+
+            //then
+            Assertions.assertThat(boardWithTagDaos)
+                .isEmpty();
+            Assertions.assertThat(allPreferenceStatisticList)
+                .hasSize(PreferenceType.values().length * boardList.size());
+        }
+
     }
 
-    @Test
-    @DisplayName("정상적으로 존재하지 않는 선호기반 게시글 통계자료를 생성한다")
-    void generateBoardPreferenceStatisticList () {
-        //given
-        preferenceStatisticService.updatingNonRankedBoards();
-
-        //when
-        List<BoardWithTagDao> boardWithTagDaos = boardRepository.checkingNullWithPreferenceRanking();
-        List<BoardPreferenceStatistic> allPreferenceStatisticList = preferenceStatisticRepository.findAll();
-
-        //then
-        Assertions.assertThat(boardWithTagDaos).isEmpty();
-        Assertions.assertThat(allPreferenceStatisticList).hasSize(PreferenceType.values().length * boardList.size());
-    }
 }
