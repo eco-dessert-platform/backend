0757f2848a1d10bc48e95c71823dfc1d2bdcaccd
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-11 15:56:18 +0900

    BoardStatistic Table과 기본 점수가 맞지 않는 데이터 가져오는 로직 추가

--- src/main/java/com/bbangle/bbangle/boardstatistic/repository/BoardPreferenceStatisticRepository.java
+++ src/main/java/com/bbangle/bbangle/boardstatistic/repository/BoardPreferenceStatisticRepository.java
@@ -1,8 +1,10 @@
 package com.bbangle.bbangle.boardstatistic.repository;
 
 import com.bbangle.bbangle.boardstatistic.domain.BoardPreferenceStatistic;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BoardPreferenceStatisticRepository extends JpaRepository<BoardPreferenceStatistic, Long> {
+public interface BoardPreferenceStatisticRepository extends JpaRepository<BoardPreferenceStatistic, Long>, BoardPreferenceStatisticQueryDSLRepository {
 
 }
