adddb0f3d1d351efeff49bb43104e65062210f51
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-11 15:55:39 +0900

    boardIds로 업데이트가 boardStatistic List를 가져오는 쿼리문 추가

--- src/main/java/com/bbangle/bbangle/boardstatistic/repository/BoardStatisticRepository.java
+++ src/main/java/com/bbangle/bbangle/boardstatistic/repository/BoardStatisticRepository.java
@@ -1,11 +1,12 @@
 package com.bbangle.bbangle.boardstatistic.repository;
 
 import com.bbangle.bbangle.boardstatistic.domain.BoardStatistic;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardStatisticRepository extends JpaRepository<BoardStatistic, Long> {
 
     Optional<BoardStatistic> findByBoardId(Long boardId);
-
+    List<BoardStatistic> findAllByBoardIds(List<Long> boardIds);
 }
