d429517f8088541c1b0d5505a2f1c3f5dfb8bdde
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-11 15:53:22 +0900

    사용하지 않는 import 삭제, 초기화 시 basicScore 맞지 않는 부분 있으면 업데이트

--- src/main/java/com/bbangle/bbangle/config/ranking/BoardStatisticConfig.java
+++ src/main/java/com/bbangle/bbangle/config/ranking/BoardStatisticConfig.java
@@ -1,19 +1,11 @@
 package com.bbangle.bbangle.config.ranking;
 
-import com.bbangle.bbangle.boardstatistic.domain.BoardStatistic;
-import com.bbangle.bbangle.boardstatistic.repository.BoardStatisticRepository;
 import com.bbangle.bbangle.boardstatistic.service.BoardPreferenceStatisticService;
 import com.bbangle.bbangle.boardstatistic.service.BoardStatisticService;
-import com.bbangle.bbangle.review.domain.QReview;
-import com.bbangle.bbangle.wishlist.domain.QWishListBoard;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.annotation.PostConstruct;
-import java.math.BigDecimal;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
@@ -30,6 +22,7 @@ public class BoardStatisticConfig {
         boardStatisticService.updatingNonRankedBoards();
         boardStatisticService.checkingStatisticStatusAndUpdate();
         preferenceStatisticService.updatingNonRankedBoards();
+        preferenceStatisticService.checkingBasicScoreAndUpdate();
     }
 
 }
