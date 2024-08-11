89889163276ee58cf075eaf57afc90d63bf5e1b4
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-11 14:39:18 +0900

    boardId 에 해당하는 boardPreferenceStatistic 정보가 없는 케이스만 잘 불러오는지 테스트

--- src/test/java/com/bbangle/bbangle/board/repository/BoardRepositoryImplTest.java
+++ src/test/java/com/bbangle/bbangle/board/repository/BoardRepositoryImplTest.java
@@ -4,18 +4,25 @@ import com.bbangle.bbangle.AbstractIntegrationTest;
 import com.bbangle.bbangle.board.dao.BoardWithTagDao;
 import com.bbangle.bbangle.board.domain.Board;
 import com.bbangle.bbangle.board.domain.Product;
+import com.bbangle.bbangle.boardstatistic.domain.BoardPreferenceStatistic;
+import com.bbangle.bbangle.boardstatistic.repository.BoardPreferenceStatisticRepository;
 import com.bbangle.bbangle.fixture.BoardFixture;
+import com.bbangle.bbangle.fixture.BoardPreferenceStatisticFixture;
 import com.bbangle.bbangle.fixture.ProductFixture;
 import com.bbangle.bbangle.fixture.StoreFixture;
 import com.bbangle.bbangle.store.domain.Store;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class BoardRepositoryImplTest extends AbstractIntegrationTest {
 
-    private Store store;
+    @Autowired
+    BoardPreferenceStatisticRepository preferenceStatisticRepository;
+
     private Board board1;
     private Board board2;
     private Product product1;
@@ -24,7 +31,7 @@ class BoardRepositoryImplTest extends AbstractIntegrationTest {
 
     @BeforeEach
     void setup(){
-        store = StoreFixture.storeGenerator();
+        Store store = StoreFixture.storeGenerator();
         store = storeRepository.save(store);
         board1 = BoardFixture.randomBoard(store);
         board2 = BoardFixture.randomBoard(store);
@@ -47,8 +54,46 @@ class BoardRepositoryImplTest extends AbstractIntegrationTest {
         List<BoardWithTagDao> boardWithTagDaos = boardRepository.checkingNullWithPreferenceRanking();
 
         //then
-        int a = 1;
-        int b = 2;
+        Assertions.assertThat(boardWithTagDaos).hasSize(3);
+        Assertions.assertThat(boardWithTagDaos.get(0).boardId()).isEqualTo(board1.getId());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().glutenFreeTag()).isEqualTo(product1.isGlutenFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().ketogenicTag()).isEqualTo(product1.isKetogenicTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().sugarFreeTag()).isEqualTo(product1.isSugarFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().highProteinTag()).isEqualTo(product1.isHighProteinTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().veganTag()).isEqualTo(product1.isVeganTag());
+        Assertions.assertThat(boardWithTagDaos.get(1).boardId()).isEqualTo(board1.getId());
+        Assertions.assertThat(boardWithTagDaos.get(1).tagsDao().glutenFreeTag()).isEqualTo(product2.isGlutenFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(1).tagsDao().ketogenicTag()).isEqualTo(product2.isKetogenicTag());
+        Assertions.assertThat(boardWithTagDaos.get(1).tagsDao().sugarFreeTag()).isEqualTo(product2.isSugarFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(1).tagsDao().highProteinTag()).isEqualTo(product2.isHighProteinTag());
+        Assertions.assertThat(boardWithTagDaos.get(1).tagsDao().veganTag()).isEqualTo(product2.isVeganTag());
+        Assertions.assertThat(boardWithTagDaos.get(2).boardId()).isEqualTo(board2.getId());
+        Assertions.assertThat(boardWithTagDaos.get(2).tagsDao().glutenFreeTag()).isEqualTo(product3.isGlutenFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(2).tagsDao().ketogenicTag()).isEqualTo(product3.isKetogenicTag());
+        Assertions.assertThat(boardWithTagDaos.get(2).tagsDao().sugarFreeTag()).isEqualTo(product3.isSugarFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(2).tagsDao().highProteinTag()).isEqualTo(product3.isHighProteinTag());
+        Assertions.assertThat(boardWithTagDaos.get(2).tagsDao().veganTag()).isEqualTo(product3.isVeganTag());
+    }
+
+    @Test
+    @DisplayName("정상적으로 게시글은 존재하지만 취향 기반의 게시글이 랭킹이 존재하지 않을 경우 정상적으로 생성한다.")
+    void makeBoardPreferenceRankingSuccessfullyExcludingAlreadyExist() {
+        //given
+        List<BoardPreferenceStatistic> basicPreferenceStatistic = BoardPreferenceStatisticFixture.createBasicPreferenceStatistic(
+            board1.getId());
+        preferenceStatisticRepository.saveAll(basicPreferenceStatistic);
+
+        // when
+        List<BoardWithTagDao> boardWithTagDaos = boardRepository.checkingNullWithPreferenceRanking();
+
+        //then
+        Assertions.assertThat(boardWithTagDaos).hasSize(1);
+        Assertions.assertThat(boardWithTagDaos.get(0).boardId()).isEqualTo(board2.getId());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().glutenFreeTag()).isEqualTo(product3.isGlutenFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().ketogenicTag()).isEqualTo(product3.isKetogenicTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().sugarFreeTag()).isEqualTo(product3.isSugarFreeTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().highProteinTag()).isEqualTo(product3.isHighProteinTag());
+        Assertions.assertThat(boardWithTagDaos.get(0).tagsDao().veganTag()).isEqualTo(product3.isVeganTag());
     }
 
 }
