67dce0e2bd8af5c0e2e8eb71741b74fee783a5b8
shoeone96 <dlwnddnjs96@gmail.com>
2024-08-13 00:45:33 +0900

    각각 update 상황에서 redis에 담는 로직으로 변경

--- src/main/java/com/bbangle/bbangle/boardstatistic/service/BoardStatisticService.java
+++ src/main/java/com/bbangle/bbangle/boardstatistic/service/BoardStatisticService.java
@@ -2,20 +2,18 @@ package com.bbangle.bbangle.boardstatistic.service;
 
 import com.bbangle.bbangle.board.domain.Board;
 import com.bbangle.bbangle.board.repository.BoardRepository;
+import com.bbangle.bbangle.boardstatistic.update.StatisticUpdate;
 import com.bbangle.bbangle.config.ranking.BoardGrade;
 import com.bbangle.bbangle.config.ranking.BoardWishCount;
-import com.bbangle.bbangle.exception.BbangleErrorCode;
-import com.bbangle.bbangle.exception.BbangleException;
 import com.bbangle.bbangle.boardstatistic.domain.BoardStatistic;
 import com.bbangle.bbangle.boardstatistic.repository.BoardStatisticRepository;
 import com.bbangle.bbangle.review.repository.ReviewRepository;
-import com.bbangle.bbangle.review.service.ReviewService;
 import com.bbangle.bbangle.wishlist.repository.WishListBoardRepository;
-import com.bbangle.bbangle.wishlist.service.WishListBoardService;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +30,7 @@ public class BoardStatisticService {
     private final BoardRepository boardRepository;
     private final ReviewRepository reviewRepository;
     private final WishListBoardRepository wishListBoardRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public void updatingNonRankedBoards() {
         List<Board> unRankedBoards = boardRepository.checkingNullRanking();
@@ -91,22 +90,15 @@ public class BoardStatisticService {
     @Async
     @Transactional
     public void updateViewCount(Long boardId) {
-        BoardStatistic boardStatistic = boardStatisticRepository.findByBoardId(boardId)
-            .orElseThrow(
-                () -> new BbangleException(BbangleErrorCode.RANKING_NOT_FOUND));
-        boardStatistic.updateViewCount();
-        boardStatistic.updateBasicScore(BOARD_VIEW_SCORE, true);
+        StatisticUpdate boardViewUpdate = StatisticUpdate.updateViewCount(boardId);
+        redisTemplate.opsForList().rightPush(String.valueOf(boardId), boardViewUpdate);
     }
 
     @Async
     @Transactional
     public void updateWishCount(Long boardId, boolean isWish) {
-        BoardStatistic boardStatistic = boardStatisticRepository.findByBoardId(boardId)
-            .orElseThrow(
-                () -> new BbangleException(BbangleErrorCode.RANKING_NOT_FOUND));
-
-        boardStatistic.updateWishCount(isWish);
-        boardStatistic.updateBasicScore(BOARD_WISH_SCORE, isWish);
+        StatisticUpdate boardWishUpdate = StatisticUpdate.updateWishCount(boardId, isWish);
+        redisTemplate.opsForList().rightPush(String.valueOf(boardId), boardWishUpdate);
     }
 
     @Async
@@ -116,12 +108,10 @@ public class BoardStatisticService {
         BigDecimal rate,
         boolean isCreate
     ) {
-        BoardStatistic boardStatistic = boardStatisticRepository.findByBoardId(boardId)
-            .orElseThrow(
-                () -> new BbangleException(BbangleErrorCode.RANKING_NOT_FOUND));
-        boardStatistic.updateReviewGrade(rate, isCreate);
-        boardStatistic.updateReviewCount(isCreate);
-        boardStatistic.updateBasicScore(BOARD_REVIEW_SCORE, isCreate);
+        StatisticUpdate ReviewRateUpdate = StatisticUpdate.updateReviewRate(boardId, rate);
+        StatisticUpdate ReviewCountUpdate = StatisticUpdate.updateReviewCount(boardId);
+        redisTemplate.opsForList().rightPush(String.valueOf(boardId), ReviewRateUpdate);
+        redisTemplate.opsForList().rightPush(String.valueOf(boardId), ReviewCountUpdate);
     }
 
 }
--- src/main/java/com/bbangle/bbangle/boardstatistic/update/StatisticUpdate.java
+++ src/main/java/com/bbangle/bbangle/boardstatistic/update/StatisticUpdate.java
@@ -2,26 +2,27 @@ package com.bbangle.bbangle.boardstatistic.update;
 
 import java.math.BigDecimal;
 
-public record StatisticUpdate (
+public record StatisticUpdate(
     UpdateType updateType,
     Long boardId,
     BigDecimal score
-){
+) {
 
-    public static StatisticUpdate updateReviewCount(Long boardId){
+    public static StatisticUpdate updateReviewCount(Long boardId) {
         return new StatisticUpdate(UpdateType.REVIEW_COUNT, boardId, null);
     }
 
-    public static StatisticUpdate updateReviewRate(Long boardId, BigDecimal reviewRate){
+    public static StatisticUpdate updateReviewRate(Long boardId, BigDecimal reviewRate) {
         return new StatisticUpdate(UpdateType.REVIEW_RATE, boardId, reviewRate);
     }
 
-    public static StatisticUpdate updateViewCount(Long boardId){
+    public static StatisticUpdate updateViewCount(Long boardId) {
         return new StatisticUpdate(UpdateType.VIEW_COUNT, boardId, null);
     }
 
-    public static StatisticUpdate updateLikeCount(Long boardId){
-        return new StatisticUpdate(UpdateType.LIKE_COUNT, boardId, null);
+    public static StatisticUpdate updateWishCount(Long boardId, Boolean isWished) {
+        BigDecimal score = Boolean.TRUE.equals(isWished) ? BigDecimal.ONE : BigDecimal.valueOf(-1);
+        return new StatisticUpdate(UpdateType.WISH_COUNT, boardId, score);
     }
 
 }
