90613513e57cacf319ef37f883b9e91d35a9c12e
shoeone96 <dlwnddnjs96@gmail.com>
2024-09-30 23:36:33 +0900

    survey 정보 업데이트, 조회 로직 추가

--- src/main/java/com/bbangle/bbangle/survey/collections/HealthConcerns.java
+++ src/main/java/com/bbangle/bbangle/survey/collections/HealthConcerns.java
@@ -2,11 +2,32 @@ package com.bbangle.bbangle.survey.collections;
 
 import com.bbangle.bbangle.survey.domain.HealthConcernInfo;
 import com.bbangle.bbangle.survey.enums.HealthConcern;
+import java.util.ArrayList;
 import java.util.List;
 
 public record HealthConcerns(
     List<HealthConcern> healthConcerns
 ) {
+
+    public static List<HealthConcern> of(HealthConcernInfo healthConcernInfo) {
+        List<HealthConcern> healthConcernList = new ArrayList<>();
+        if(healthConcernInfo.getPimples()){
+            healthConcernList.add(HealthConcern.PIMPLES);
+        }
+        if(healthConcernInfo.getBodyFat()){
+            healthConcernList.add(HealthConcern.BODY_FAT);
+        }
+        if(healthConcernInfo.getCholesterol()){
+            healthConcernList.add(HealthConcern.CHOLESTEROL);
+        }
+        if(healthConcernInfo.getNotApplicable()){
+            healthConcernList.add(HealthConcern.NOT_APPLICABLE);
+        }
+        if(healthConcernInfo.getDigestiveDisorder()){
+            healthConcernList.add(HealthConcern.DIGESTIVE_DISORDER);
+        }
+        return healthConcernList;
+    }
 
     public HealthConcernInfo getInfo() {
         boolean pimples = false;
