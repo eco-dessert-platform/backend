3b8fdb808dab84499df02bb06c944c2b143eff48
shoeone96 <dlwnddnjs96@gmail.com>
2024-10-12 14:58:27 +0900

    GC로 잡히지 않는 LinkedHashMap clear 시키기

--- src/main/java/com/bbangle/bbangle/board/repository/util/BoardPageGenerator.java
+++ src/main/java/com/bbangle/bbangle/board/repository/util/BoardPageGenerator.java
@@ -64,8 +64,13 @@ public class BoardPageGenerator {
 
         boardResponseDaoList = removeDuplicatesByBoardId(boardResponseDaoList);
 
-        return getBoardResponseDtos(boardResponseDaoList, isInFolder, isBundled, tagMapByBoardId,
+        List<BoardResponseDto> boardResponseDtos = getBoardResponseDtos(boardResponseDaoList,
+            isInFolder, isBundled, tagMapByBoardId,
             isSoldOut, isBbangcketing);
+
+        clearLinkedHashMap(tagMapByBoardId, isBundled, isSoldOut, isBbangcketing);
+
+        return boardResponseDtos;
     }
 
     private static Map<Long, Boolean> getIsBbangcketing(List<BoardResponseDao> boardResponseDaoList) {
@@ -224,6 +229,18 @@ public class BoardPageGenerator {
         if (condition) {
             tags.add(tag);
         }
+    }
+
+    private static void clearLinkedHashMap(
+        Map<Long, List<String>> tagMapByBoardId,
+        Map<Long, Boolean> isBundled,
+        Map<Long, Boolean> isSoldOut,
+        Map<Long, Boolean> isBbangcketing
+    ) {
+        tagMapByBoardId.clear();
+        isBundled.clear();
+        isSoldOut.clear();
+        isBbangcketing.clear();
     }
 
 }
