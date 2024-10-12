1641898f7d14566292229ef62d000d8f6e29455c
shoeone96 <dlwnddnjs96@gmail.com>
2024-10-12 17:29:55 +0900

    캐싱 configuration 적용

--- src/main/java/com/bbangle/bbangle/BbangleApplication.java
+++ src/main/java/com/bbangle/bbangle/BbangleApplication.java
@@ -3,9 +3,11 @@ package com.bbangle.bbangle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableCaching
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BbangleApplication {
