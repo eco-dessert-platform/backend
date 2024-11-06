9a8254bcc194d009e1ca9ce90d4cc4bd13b75a4d
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-11-07 01:09:47 +0900

    반복 Map 정리 및 BoardInfo Dto 클래스 구현

--- src/main/java/com/bbangle/bbangle/board/service/BoardDetailService.java
+++ src/main/java/com/bbangle/bbangle/board/service/BoardDetailService.java
@@ -1,15 +1,13 @@
 package com.bbangle.bbangle.board.service;
 
 import com.bbangle.bbangle.board.common.TagUtils;
-import com.bbangle.bbangle.board.dao.TagsDao;
-import com.bbangle.bbangle.board.domain.Category;
+import com.bbangle.bbangle.board.dto.BoardInfo;
 import com.bbangle.bbangle.board.dto.SimilarityBoardDto;
 import com.bbangle.bbangle.board.dto.SimilarityBoardResponse;
 import com.bbangle.bbangle.board.repository.BoardDetailRepository;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,68 +18,42 @@ public class BoardDetailService {
     private final BoardDetailRepository boardDetailRepository;
 
     public List<SimilarityBoardResponse> getSimilarityBoardResponses(Long memberId, Long boardId) {
-        List<SimilarityBoardDto> similarityBoardDtos = boardDetailRepository.findSimilarityBoardByBoardId(
-            memberId,
-            boardId);
+        List<SimilarityBoardDto> similarityBoardDtos = boardDetailRepository.findSimilarityBoardByBoardId(memberId, boardId);
 
-        Map<Long, List<TagsDao>> categorizedTagByBoardId = similarityBoardDtos.stream()
-            .collect(Collectors.groupingBy(
-                SimilarityBoardDto::getBoardId,
-                Collectors.mapping(SimilarityBoardDto::getTagsDao,
-                    Collectors.toList())
-            ));
+        Map<Long, BoardInfo> boardInfoMap = new HashMap<>();
+        for (SimilarityBoardDto dto : similarityBoardDtos) {
+            Long currentBoardId = dto.getBoardId();
 
-        Map<Long, Set<Category>> categorizedCategoryByBoardId = similarityBoardDtos.stream()
-            .collect(Collectors.groupingBy(
-                SimilarityBoardDto::getBoardId,
-                Collectors.mapping(SimilarityBoardDto::getCategory,
-                    Collectors.toSet())
-            ));
+            BoardInfo boardInfo = boardInfoMap.computeIfAbsent(currentBoardId, id -> new BoardInfo(dto));
 
-        Map<Long, Set<Boolean>> categorizedIsSoldOutByBoardId = similarityBoardDtos.stream()
-            .collect(Collectors.groupingBy(
-                SimilarityBoardDto::getBoardId,
-                Collectors.mapping(SimilarityBoardDto::getIsSoldOut,
-                    Collectors.toSet())
-            ));
+            boardInfo.addTag(dto.getTagsDao());
+            boardInfo.addCategory(dto.getCategory());
+            boardInfo.addIsSoldOut(dto.getIsSoldOut());
+        }
 
-        return similarityBoardDtos.stream()
-            .collect(Collectors.toMap(
-                SimilarityBoardDto::getBoardId, // 키는 getBoardId 값
-                dto -> dto,                      // 값은 원래 dto 객체
-                (existing, replacement) -> existing // 중복 키가 있을 경우 기존 값 유지
-            ))
-            .values()
-            .stream()
-            .map(similarityBoardDto -> {
-                Long boardId1 = similarityBoardDto.getBoardId();
-
-                List<TagsDao> tagsDaos = categorizedTagByBoardId.get(boardId1);
-                Set<Category> categories = categorizedCategoryByBoardId.get(boardId1);
-                Set<Boolean> soldout = categorizedIsSoldOutByBoardId.get(boardId1);
-
-                List<String> tags = TagUtils.convertToStrings(tagsDaos);
-                Boolean isBundled = categories.size() > 1;
-                Boolean isSoldOut = soldout.size() == 1 && soldout.iterator().next() == true;
+        return boardInfoMap.values().stream()
+            .map(boardInfo -> {
+                List<String> tags = TagUtils.convertToStrings(boardInfo.getTags());
+                Boolean isBundled = boardInfo.getCategories().size() > 1;
+                Boolean isSoldOut = !boardInfo.getIsSoldOut().contains(false);
 
                 return SimilarityBoardResponse.builder()
-                    .boardId(similarityBoardDto.getBoardId())
-                    .storeId(similarityBoardDto.getStoreId())
-                    .thumbnail(similarityBoardDto.getThumbnail())
-                    .storeName(similarityBoardDto.getStoreName())
-                    .title(similarityBoardDto.getTitle())
-                    .discountRate(similarityBoardDto.getDiscountRate())
-                    .price(similarityBoardDto.getPrice())
-                    .reviewRate(similarityBoardDto.getReviewRate())
-                    .reviewCount(similarityBoardDto.getReviewCount())
+                    .boardId(boardInfo.getBoardId())
+                    .storeId(boardInfo.getStoreId())
+                    .thumbnail(boardInfo.getThumbnail())
+                    .storeName(boardInfo.getStoreName())
+                    .title(boardInfo.getTitle())
+                    .discountRate(boardInfo.getDiscountRate())
+                    .price(boardInfo.getPrice())
+                    .reviewRate(boardInfo.getReviewRate())
+                    .reviewCount(boardInfo.getReviewCount())
                     .tags(tags)
-                    .isWished(similarityBoardDto.getIsWished())
+                    .isWished(boardInfo.getIsWished())
                     .isBundled(isBundled)
                     .isSoldOut(isSoldOut)
-                    .similarityType(similarityBoardDto.getSimilarityType())
+                    .similarityType(boardInfo.getSimilarityType())
                     .build();
             }).toList();
     }
-
 }
 
