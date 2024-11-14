32b3b6f08393b7e52e2dff6094cebed316c12e38
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-11-15 08:52:04 +0900

    최적화 진행

--- src/test/java/com/bbangle/bbangle/board/service/solution/SolutionContext.java
+++ src/test/java/com/bbangle/bbangle/board/service/solution/SolutionContext.java
@@ -1,8 +1,8 @@
 package com.bbangle.bbangle.board.service.solution;
 
-import com.bbangle.bbangle.board.service.domain.SolutionEnum;
-import java.util.ArrayList;
+import com.bbangle.bbangle.board.service.solution.domain.SolutionEnum;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Builder;
@@ -12,13 +12,21 @@ import lombok.Getter;
 public class SolutionContext {
     List<SolutionEnum> solutionEnums;
 
-    List<Map<Long, EnumMap<SolutionEnum, List<Object>>>> data;
-    List<Map<Long, EnumMap<SolutionEnum, Object>>> outputData;
+    Map<Long, EnumMap<SolutionEnum, List<Object>>> data;
+    Map<Long, EnumMap<SolutionEnum, Object>> outputData;
 
     @Builder
     public SolutionContext(List<SolutionEnum> solutionEnums) {
         this.solutionEnums = solutionEnums;
-        data = new ArrayList<>();
-        outputData = new ArrayList<>();
+        data = new HashMap<>();
+        outputData = new HashMap<>();
+    }
+
+    void setData(Map<Long, EnumMap<SolutionEnum, List<Object>>> data) {
+        this.data = data;
+    }
+
+    void setOutputData(Map<Long, EnumMap<SolutionEnum, Object>> outputData) {
+        this.outputData = outputData;
     }
 }
