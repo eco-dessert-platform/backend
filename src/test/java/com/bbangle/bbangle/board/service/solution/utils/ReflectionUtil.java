aac524cab00f8302776778a53143c93838487cc9
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-11-15 08:50:13 +0900

    리플렉션 캐시 추가

--- src/test/java/com/bbangle/bbangle/board/service/solution/utils/ReflectionUtil.java
+++ src/test/java/com/bbangle/bbangle/board/service/solution/utils/ReflectionUtil.java
@@ -1,17 +1,35 @@
 package com.bbangle.bbangle.board.service.solution.utils;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReflectionUtil {
+    private static final Map<Class<?>, Map<String, Field>> fieldCache = new ConcurrentHashMap<>();
+
     public static <T, U> T getField(U instance, String name, Class<T> t) {
         try {
-            Class<?> dtoClass = instance.getClass();
-            var field = dtoClass.getDeclaredField(name);
+            Field field = getCachedField(instance.getClass(), name);
             field.setAccessible(true);
 
             return t.cast(field.get(instance));
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Field getCachedField(Class<?> clazz, String fieldName) throws NoSuchFieldException {
+        Map<String, Field> classFieldMap = fieldCache.computeIfAbsent(clazz, k -> new ConcurrentHashMap<>());
+
+        return classFieldMap.computeIfAbsent(fieldName, name -> {
+            try {
+                return clazz.getDeclaredField(name);
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
