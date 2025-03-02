d7cfa861a2364285a3d6670377c4ad2fcf08b955
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2025-03-03 01:26:53 +0900

    [Fix] 스토어 조회, 속도 문제 해결
    로직이 아주 지저분하게 짜여있어서 매우 느린 속도를 보였음

--- src/main/java/com/bbangle/bbangle/store/repository/StoreRepositoryImpl.java
+++ src/main/java/com/bbangle/bbangle/store/repository/StoreRepositoryImpl.java
@@ -1,15 +1,12 @@
 package com.bbangle.bbangle.store.repository;
 
-import com.bbangle.bbangle.board.domain.QBoard;
+import static com.bbangle.bbangle.board.domain.QBoard.board;
+import static com.bbangle.bbangle.store.domain.QStore.store;
+
 import com.bbangle.bbangle.board.dto.AiLearningStoreDto;
 import com.bbangle.bbangle.board.dto.QAiLearningStoreDto;
-import com.bbangle.bbangle.store.dto.QStoreDetailStoreDto;
 import com.bbangle.bbangle.store.dto.QStoreDto;
-import com.bbangle.bbangle.store.domain.QStore;
-import com.bbangle.bbangle.store.dto.StoreDetailStoreDto;
 import com.bbangle.bbangle.store.dto.StoreDto;
-import com.bbangle.bbangle.wishlist.domain.QWishListStore;
-import com.bbangle.bbangle.wishlist.repository.util.WishListStoreFilter;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -19,30 +16,7 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class StoreRepositoryImpl implements StoreQueryDSLRepository {
 
-    private static final QStore store = QStore.store;
-    private static final QBoard board = QBoard.board;
-    private static final QWishListStore wishListStore = QWishListStore.wishListStore;
-
     private final JPAQueryFactory queryFactory;
-    private final WishListStoreFilter wishListStoreFilter;
-
-    @Override
-    public StoreDetailStoreDto getStoreResponse(Long memberId, Long storeId) {
-        return queryFactory.select(
-                new QStoreDetailStoreDto(
-                    store.id,
-                    store.profile,
-                    store.name,
-                    store.introduce,
-                    wishListStore.id)
-            ).from(store)
-            .where(store.id.eq(storeId))
-            .leftJoin(wishListStore)
-            .on(wishListStoreFilter.equalMemberId(memberId)
-                .and(wishListStoreFilter.equalStoreId(store))
-                .and(wishListStore.isDeleted.isFalse()))
-            .fetchOne();
-    }
 
     @Override
     public StoreDto findByBoardId(Long boardId) {
