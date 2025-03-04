8401610a189c5605d79abe5359459afa9d6cd40c
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2025-03-04 17:39:01 +0900

    Main Merge  (#355)
    Co-authored-by: sunwon <92251131+sunwon12@users.noreply.github.com>

--- src/main/java/com/bbangle/bbangle/config/WebConfig.java
+++ src/main/java/com/bbangle/bbangle/config/WebConfig.java
@@ -1,22 +1,36 @@
 package com.bbangle.bbangle.config;
 
 import java.util.List;
+
+import com.bbangle.bbangle.common.service.RequestTimeInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private OctetStreamReadMsgConverter octetStreamReadMsgConverter;
 
-    @Autowired
-    public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
-        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
-    }
+  private final RequestTimeInterceptor requestTimeInterceptor;
+  private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(octetStreamReadMsgConverter);
-    }
+  @Autowired
+  public WebConfig(
+      OctetStreamReadMsgConverter octetStreamReadMsgConverter,
+      RequestTimeInterceptor requestTimeInterceptor) {
+    this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    this.requestTimeInterceptor = requestTimeInterceptor;
+  }
+
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(octetStreamReadMsgConverter);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(requestTimeInterceptor);
+  }
+
 }
