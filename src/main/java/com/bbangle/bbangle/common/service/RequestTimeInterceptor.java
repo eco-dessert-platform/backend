0ab81edc086e2a84d161179258afdf223e8a3c41
yunyechan <dpcks9893@naver.com>
2025-03-05 18:17:27 +0900

    [Refactor] 3초 이상 걸리는 API 슬랙으로 정보 전송

--- src/main/java/com/bbangle/bbangle/common/service/RequestTimeInterceptor.java
+++ src/main/java/com/bbangle/bbangle/common/service/RequestTimeInterceptor.java
@@ -1,14 +1,21 @@
 package com.bbangle.bbangle.common.service;
 
+import com.bbangle.bbangle.common.adaptor.slack.SlackAdaptor;
+import com.bbangle.bbangle.exception.BbangleException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class RequestTimeInterceptor implements HandlerInterceptor {
+
+  private static final Long THREE_SECONDS = 3000L;
+  private final SlackAdaptor slackAdaptor;
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
@@ -19,10 +26,19 @@ public class RequestTimeInterceptor implements HandlerInterceptor {
 
   @Override
   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+
     long startTime = (Long) request.getAttribute("startTime");
     long endTime = System.currentTimeMillis();
     long duration = endTime - startTime;
 
-    log.info("요청 처리 시간: " + duration + "ms");
+    String requestURI = request.getRequestURI();
+    String requestMethod = request.getMethod();
+
+    if (duration > THREE_SECONDS) {
+      String message = String.format("요청 [%s %s] 처리 시간: %dms", requestMethod, requestURI, duration);
+      slackAdaptor.sendAlert(request, new BbangleException(message));
+      log.info(message);
+    }
   }
 }
