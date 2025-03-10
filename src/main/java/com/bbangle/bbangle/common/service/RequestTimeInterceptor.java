98e8718cd372e987adb200e2b57748019ba7b72c
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2025-03-10 19:18:00 +0900

    [Feat] 개발서버 스웨거 알림 구현 (#359)

--- src/main/java/com/bbangle/bbangle/common/service/RequestTimeInterceptor.java
+++ src/main/java/com/bbangle/bbangle/common/service/RequestTimeInterceptor.java
@@ -14,31 +14,33 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @RequiredArgsConstructor
 public class RequestTimeInterceptor implements HandlerInterceptor {
 
-  private static final Long THREE_SECONDS = 3000L;
-  private final SlackAdaptor slackAdaptor;
+    private static final Long THREE_SECONDS = 3000L;
+    private final SlackAdaptor slackAdaptor;
 
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-    long startTime = System.currentTimeMillis();
-    request.setAttribute("startTime", startTime); // 요청 속성에 저장
-    return true;
-  }
-
-  @Override
-  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-
-
-    long startTime = (Long) request.getAttribute("startTime");
-    long endTime = System.currentTimeMillis();
-    long duration = endTime - startTime;
-
-    String requestURI = request.getRequestURI();
-    String requestMethod = request.getMethod();
-
-    if (duration > THREE_SECONDS) {
-      String message = String.format("요청 [%s %s] 처리 시간: %dms", requestMethod, requestURI, duration);
-      slackAdaptor.sendAlert(request, new BbangleException(message));
-      log.info(message);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return true;
     }
-  }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+        Object handler, Exception ex) {
+
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        String requestURI = request.getRequestURI();
+        String requestMethod = request.getMethod();
+
+        if (duration > THREE_SECONDS) {
+            String header = String.format("요청 [%s %s]", requestMethod, requestURI);
+            String content = String.format("처리 시간: %dms", duration);
+            slackAdaptor.sendText(header, content);
+            log.debug(header + header);
+        }
+    }
 }
