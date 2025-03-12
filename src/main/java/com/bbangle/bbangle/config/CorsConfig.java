6bb09431f3f52964da920d38c8498343c7e56fcc
sunwon <jsw5913@naver.com>
2025-03-12 14:30:14 +0900

    [Refactor] cors prod,dev 분할
    - prod에서 스웨거 접속을 차단하기 위함

--- src/main/java/com/bbangle/bbangle/config/CorsConfig.java
+++ src/main/java/com/bbangle/bbangle/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.bbangle.bbangle.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,25 +10,45 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    @Profile({"dev", "local"})
+    public WebMvcConfigurer devCorsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOriginPatterns(
-                        "http://localhost:5000",
-                        "http://localhost:3000",
-                        "http://localhost:63342",
-                        "http://localhost:8001",
-                        "https://dev.bbanggree.com",
-                        "https://www.bbanggree.com",
-                        "https://api.bbanggree.com",
-                        "https://master.d2xvuesi0d3ssg.amplifyapp.com"
-                    )
-                    .allowedHeaders("*")
-                    .exposedHeaders("ACCESS_KEY", "Authorization", "RefreshToken")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
-                    .allowCredentials(true);
+                        .allowedOriginPatterns(
+                                "http://localhost:5000",
+                                "http://localhost:3000",
+                                "http://localhost:63342",
+                                "http://localhost:8001",
+                                "http://dev.bbanggree.com",
+                                "https://dev.bbanggree.com",
+                                "https://master.d2xvuesi0d3ssg.amplifyapp.com"
+                        )
+                        .allowedHeaders("*")
+                        .exposedHeaders("ACCESS_KEY", "Authorization", "RefreshToken")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    @Bean
+    @Profile("production")
+    public WebMvcConfigurer prodCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns(
+                                "https://www.bbanggree.com",
+                                "https://api.bbanggree.com",
+                                "https://master.d2xvuesi0d3ssg.amplifyapp.com"
+                        )
+                        .allowedHeaders("*")
+                        .exposedHeaders("ACCESS_KEY", "Authorization", "RefreshToken")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+                        .allowCredentials(true);
             }
         };
     }
