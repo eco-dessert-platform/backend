cbc3b0a45d3f5f9d67a78088e448da0ee2bf199c
sunwon <jsw5913@naver.com>
2025-03-18 16:21:05 +0900

    [Remove] 전략패턴으로 바꿈으로써 이전 resolver 클래스들 삭제

--- src/main/java/com/bbangle/bbangle/board/repository/folder/cursor/BoardInFolderCursorGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.bbangle.bbangle.board.repository.folder.cursor;
-
-import com.querydsl.core.BooleanBuilder;
-
-public interface BoardInFolderCursorGenerator {
-
-    BooleanBuilder getCursor(Long cursorId, Long folderId);
-
-}
