58665c72232fa54b9ff266d5ba43a38a7e9a0356
yunyechan <dpcks9893@naver.com>
2025-03-22 23:53:34 +0900

    [Refactor] 게시판 검색 리팩토링

--- src/main/java/com/bbangle/bbangle/search/repository/SearchRepositoryImpl.java
+++ src/main/java/com/bbangle/bbangle/search/repository/SearchRepositoryImpl.java
@@ -1,22 +1,24 @@
 package com.bbangle.bbangle.search.repository;
 
 import com.bbangle.bbangle.board.domain.Board;
-import com.bbangle.bbangle.board.dto.FilterRequest;
-import com.bbangle.bbangle.board.repository.basic.cursor.BoardCursorGeneratorMapping;
+import com.bbangle.bbangle.exception.BbangleErrorCode;
+import com.bbangle.bbangle.exception.BbangleException;
 import com.bbangle.bbangle.search.dto.KeywordDto;
 import com.bbangle.bbangle.search.dto.QKeywordDto;
+import com.bbangle.bbangle.search.repository.component.SearchFilter;
+import com.bbangle.bbangle.search.repository.component.SearchSort;
+import com.bbangle.bbangle.search.service.dto.QSearchInfo_CursorCondition;
 import com.bbangle.bbangle.search.service.dto.SearchCommand;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.OrderSpecifier;
+import com.bbangle.bbangle.search.service.dto.SearchInfo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static com.bbangle.bbangle.board.domain.QBoard.board;
-import static com.bbangle.bbangle.board.domain.QProduct.product;
 import static com.bbangle.bbangle.board.repository.BoardRepositoryImpl.BOARD_PAGE_SIZE;
 import static com.bbangle.bbangle.boardstatistic.domain.QBoardStatistic.boardStatistic;
 import static com.bbangle.bbangle.search.domain.QSearch.search;
@@ -26,86 +28,103 @@ import static com.bbangle.bbangle.store.domain.QStore.store;
 @RequiredArgsConstructor
 public class SearchRepositoryImpl implements SearchQueryDSLRepository {
 
-    private static final Integer BOARD_PAGE_SIZE_PLUS_ONE = BOARD_PAGE_SIZE + 1;
+        private static final Integer BOARD_PAGE_SIZE_PLUS_ONE = BOARD_PAGE_SIZE + 1;
 
-    private final BoardCursorGeneratorMapping boardCursorGeneratorMapping;
-    private final JPAQueryFactory queryFactory;
+        private final JPAQueryFactory queryFactory;
+        private final SearchFilter searchFilter;
+        private final SearchSort searchSort;
 
+        @Override
+        public SearchInfo.CursorCondition getCursorCondition(Long cursorId) {
+                return Optional.ofNullable(
+                        queryFactory.select(
+                                new QSearchInfo_CursorCondition(
+                                    board.id,
+                                    boardStatistic.basicScore,
+                                    board.price,
+                                    boardStatistic.boardWishCount,
+                                    boardStatistic.boardReviewCount))
+                            .from(boardStatistic)
+                            .join(boardStatistic.board, board)
+                            .where(board.id.eq(cursorId))
+                            .fetchOne())
+                    .orElseThrow(() -> new BbangleException(
+                        BbangleErrorCode.RANKING_NOT_FOUND));
+        }
 
-    @Override
-    public List<Board> getBoardResponseList(SearchCommand.Main command) {
-        BooleanBuilder filter = new SearchFilterCreator(command.keyword(), command.filterRequest()).create();
-        BooleanBuilder cursorInfo = boardCursorGeneratorMapping.mappingCursorGenerator(command.sort())
-            .getCursor(command.cursorId());
-        OrderSpecifier<?>[] orderExpression = command.sort().getOrderExpression();
+        @Override
+        public List<Board> getBoardResponseList(SearchCommand.Main command, SearchInfo.CursorCondition condition) {
+                return queryFactory.selectFrom(board)
+                    .join(board.store, store).fetchJoin()
+                    .leftJoin(board.boardStatistic, boardStatistic).fetchJoin()
+                    .where(
+                        searchFilter.getLikeKeyword(command.keyword()),
+                        searchFilter.getEqualTag(command.filterRequest()),
+                        searchFilter.getBetweenPrice(command.filterRequest()),
+                        searchFilter.getCategory(command.filterRequest()),
+                        searchFilter.getCursorCondition(command.sort(), condition),
+                        board.isDeleted.isFalse()
+                    )
+                    .orderBy(searchSort.getSortType(command.sort()))
+                    .limit(BOARD_PAGE_SIZE_PLUS_ONE)
+                    .fetch();
+        }
 
-        return queryFactory.selectFrom(board)
-            .join(board.store, store).fetchJoin()
-            .leftJoin(board.products, product).fetchJoin()
-            .leftJoin(board.boardStatistic, boardStatistic).fetchJoin()
-            .where(
-                cursorInfo,
-                filter)
-            .orderBy(orderExpression)
-            .limit(BOARD_PAGE_SIZE_PLUS_ONE)
-            .fetch();
-    }
+        @Override
+        public Long getAllCount(
+            SearchCommand.Main command,
+            SearchInfo.CursorCondition condition
+        ) {
+                return queryFactory.select(board.count())
+                    .from(board)
+                    .where(
+                        searchFilter.getLikeKeyword(command.keyword()),
+                        searchFilter.getEqualTag(command.filterRequest()),
+                        searchFilter.getBetweenPrice(command.filterRequest()),
+                        searchFilter.getCategory(command.filterRequest()),
+                        searchFilter.getCursorCondition(command.sort(), condition),
+                        board.isDeleted.isFalse()
+                    ).fetchOne();
+        }
 
-    @Override
-    public Long getAllCount(
-        String keyword,
-        FilterRequest filterRequest
-    ) {
-        BooleanBuilder filter = new SearchFilterCreator(keyword, filterRequest).create();
+        @Override
+        public List<KeywordDto> getRecencyKeyword(Long memberId) {
+                return queryFactory.select(
+                        new QKeywordDto(
+                            search.keyword))
+                    .distinct()
+                    .from(search)
+                    .where(
+                        search.isDeleted.eq(false),
+                        search.member.id.eq(memberId))
+                    .orderBy(search.id.desc())
+                    .limit(7)
+                    .fetch();
+        }
 
-        return queryFactory.select(board.id)
-            .distinct()
-            .from(product)
-            .join(board)
-            .on(product.board.id.eq(board.id))
-            .join(board.boardStatistic, boardStatistic)
-            .where(filter)
-            .fetch().stream().count();
-    }
+        @Override
+        public String[] getBestKeyword(
+            LocalDateTime beforeOneDayTime
+        ) {
+                return queryFactory.select(search.keyword)
+                    .from(search)
+                    .where(search.createdAt.gt(beforeOneDayTime))
+                    .groupBy(search.keyword)
+                    .orderBy(search.count().desc())
+                    .limit(7)
+                    .fetch()
+                    .toArray(new String[0]);
+        }
 
-    @Override
-    public List<KeywordDto> getRecencyKeyword(Long memberId) {
-        return queryFactory.select(
-                new QKeywordDto(
-                    search.keyword))
-            .distinct()
-            .from(search)
-            .where(
-                search.isDeleted.eq(false),
-                search.member.id.eq(memberId))
-            .orderBy(search.id.desc())
-            .limit(7)
-            .fetch();
-    }
-
-    @Override
-    public String[] getBestKeyword(
-        LocalDateTime beforeOneDayTime
-    ) {
-        return queryFactory.select(search.keyword)
-            .from(search)
-            .where(search.createdAt.gt(beforeOneDayTime))
-            .groupBy(search.keyword)
-            .orderBy(search.count().desc())
-            .limit(7)
-            .fetch()
-            .toArray(new String[0]);
-    }
-
-    @Override
-    public void markAsDeleted(String keyword, Long memberId) {
-        queryFactory.update(search)
-            .set(search.isDeleted, true)
-            .where(
-                search.member.id.eq(memberId)
-                    .and(search.keyword.eq(keyword))
-            )
-            .execute();
-    }
+        @Override
+        public void markAsDeleted(String keyword, Long memberId) {
+                queryFactory.update(search)
+                    .set(search.isDeleted, true)
+                    .where(
+                        search.member.id.eq(memberId)
+                            .and(search.keyword.eq(keyword))
+                    )
+                    .execute();
+        }
 
 }
--- src/main/java/com/bbangle/bbangle/search/service/dto/SearchInfo.java
+++ src/main/java/com/bbangle/bbangle/search/service/dto/SearchInfo.java
@@ -1,38 +1,70 @@
 package com.bbangle.bbangle.search.service.dto;
 
+import com.bbangle.bbangle.board.sort.SortType;
 import com.bbangle.bbangle.search.dto.SearchBoardResponseDto;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 public class SearchInfo {
 
-    @Data
-    @AllArgsConstructor
-    public class Select {
-        private Long boardId;
-        private Long storeId;
-        private String storeName;
-        private String thumbnail;
-        private String title;
-        private Integer price;
-        private Boolean isBundled;
-        private List<String> tags;
-        private BigDecimal reviewRate;
-        private Long reviewCount;
-        private Boolean isBbangcketing;
-        private Boolean isSoldOut;
-        private Integer discountRate;
-        private Boolean isWished;
-    }
+        @Data
+        @AllArgsConstructor
+        public static class Select {
+                private Long boardId;
+                private Long storeId;
+                private String storeName;
+                private String thumbnail;
+                private String title;
+                private Integer price;
+                private Boolean isBundled;
+                private List<String> tags;
+                private BigDecimal reviewRate;
+                private Long reviewCount;
+                private Boolean isBbangcketing;
+                private Boolean isSoldOut;
+                private Integer discountRate;
+                private Boolean isWished;
+        }
 
-    @Data
-    @AllArgsConstructor
-    public static class SearchBoardPage{
-        private List<SearchInfo.Select> boards;
-        private Long allItemCount;
-    }
+        @Data
+        @AllArgsConstructor
+        public static class SearchBoardPage {
+                private List<SearchInfo.Select> boards;
+                private Long allItemCount;
+        }
+
+        @Data
+        @NoArgsConstructor
+        public static class CursorCondition {
+                private Long cursorId;
+                private Double boardBasicScore;
+                private Integer price;
+                private Long boardWishedCount;
+                private Long boardReviewCount;
+
+                public CursorCondition(Long cursorId) {
+                        this.cursorId = cursorId;
+                }
+
+                @QueryProjection
+                public CursorCondition(Long cursorId, Double boardBasicScore, Integer price, Long boardWishedCount, Long boardReviewCount) {
+                        this.cursorId = cursorId;
+                        this.boardBasicScore = boardBasicScore;
+                        this.price = price;
+                        this.boardWishedCount = boardWishedCount;
+                        this.boardReviewCount = boardReviewCount;
+                }
+
+                public static CursorCondition empty() {
+                        return new CursorCondition(Long.MAX_VALUE, 0.0, 0, 0L, 0L);
+                }
+
+        }
 
 }
