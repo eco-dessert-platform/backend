8c96f0cb6e860eb661fe769dea310290b81ebfd5
kmindev <kangkm0324@gmail.com>
2025-05-20 22:27:04 +0900

    [Style] 공백 제거, 라인 정리

--- src/main/java/com/bbangle/bbangle/member/domain/Role.java
+++ src/main/java/com/bbangle/bbangle/member/domain/Role.java
@@ -8,11 +8,10 @@ public enum Role {
 
     ROLE_USER("ROLE_USER", "사용자"),
     ROLE_SELLER("ROLE_SELLER", "판매자"),
-    ROLE_ADMIN("ROLE_ADMIN", "빵그리오븐관리자 ");
+    ROLE_ADMIN("ROLE_ADMIN", "빵그리오븐관리자");
 
     private final String role;
     private final String description;
-
 
     Role(String role, String description) {
         this.role = role;
@@ -25,5 +24,5 @@ public enum Role {
                 .findFirst()
                 .orElse(ROLE_USER);
     }
-    
+
 }
