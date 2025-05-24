c60df26c9cac4c1548a3eeb35e06952926fadc46
Kang KyeongMin <kangkm0324@gmail.com>
2025-05-24 09:32:46 +0900

    [Fix] No constructor found ERROR 수정   (#395)

--- src/main/java/com/bbangle/bbangle/member/dto/MemberIdWithRoleDto.java
+++ src/main/java/com/bbangle/bbangle/member/dto/MemberIdWithRoleDto.java
@@ -2,12 +2,11 @@ package com.bbangle.bbangle.member.dto;
 
 import com.bbangle.bbangle.member.domain.Member;
 import com.bbangle.bbangle.member.domain.Role;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class MemberIdWithRoleDto {
 
     private Long memberId;
