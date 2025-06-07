a6b183564a7d5814450887332acc974d7182641d
kmindev <kangkm0324@gmail.com>
2025-06-07 17:04:20 +0900

    [Feat] 공지사항 관련 Swagger 명세 개선

--- src/main/java/com/bbangle/bbangle/notification/controller/NotificationController.java
+++ src/main/java/com/bbangle/bbangle/notification/controller/NotificationController.java
@@ -8,11 +8,14 @@ import com.bbangle.bbangle.notification.dto.NotificationResponse;
 import com.bbangle.bbangle.notification.dto.NotificationUploadRequest;
 import com.bbangle.bbangle.notification.service.NotificationService;
 import com.bbangle.bbangle.common.page.NotificationCustomPage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Notification", description = "공지사항 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notification")
@@ -21,6 +24,7 @@ public class NotificationController {
     private final NotificationService notificationService;
     private final ResponseService responseService;
 
+    @Operation(summary = "공지사항 목록 조회")
     @GetMapping
     public SingleResult<NotificationCustomPage<List<NotificationResponse>>> getList(
         @RequestParam(required = false, value = "cursorId")
@@ -29,6 +33,7 @@ public class NotificationController {
         return responseService.getSingleResult(notificationService.getList(cursorId));
     }
 
+    @Operation(summary = "공지사항 상세 조회")
     @GetMapping("/{id}")
     public SingleResult<NotificationDetailResponseDto> getNoticeDetail(
         @PathVariable
@@ -37,6 +42,7 @@ public class NotificationController {
         return responseService.getSingleResult(notificationService.getNoticeDetail(id));
     }
 
+    @Operation(summary = "공지사항 등록")
     @PostMapping
     public CommonResult upload(
         @RequestBody
