21f7c50198ec2eebbd825d917e049aadb537bd5d
sunwon <92251131+sunwon12@users.noreply.github.com>
2025-06-10 20:54:35 +0900

    [Chore] 스웨거에 API 설명 추가 (#405)

--- src/main/java/com/bbangle/bbangle/token/oauth/OauthController.java
+++ src/main/java/com/bbangle/bbangle/token/oauth/OauthController.java
@@ -5,6 +5,7 @@ import com.bbangle.bbangle.common.service.ResponseService;
 import com.bbangle.bbangle.token.jwt.TokenCookieProvider;
 import com.bbangle.bbangle.token.oauth.domain.OauthServerType;
 import com.bbangle.bbangle.token.oauth.infra.kakao.dto.LoginTokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +24,13 @@ public class OauthController {
     private final TokenCookieProvider tokenCookieProvider;
 
     @GetMapping("/login/{oauthServerType}")
+    @Operation(summary = "Oauth 로그인")
     CommonResult login(
         @PathVariable("oauthServerType") OauthServerType oauthServerType,
-        @RequestParam("token") String token,
+        @RequestParam String tokenByOauth,
         HttpServletResponse response
     ) {
-        LoginTokenResponse loginTokenResponse = oauthService.login(oauthServerType, token);
+        LoginTokenResponse loginTokenResponse = oauthService.login(oauthServerType, tokenByOauth);
         
         tokenCookieProvider.addAccessTokenCookie(response, loginTokenResponse.accessToken());
         tokenCookieProvider.addRefreshTokenCookie(response, loginTokenResponse.refreshToken());
