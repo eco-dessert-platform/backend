8608269964a33ad291c2a9898f49f8b7846678bd
sunwon <92251131+sunwon12@users.noreply.github.com>
2025-07-04 22:00:22 +0900

    [Refactor] JWT로 롤백 (#407)

--- src/main/java/com/bbangle/bbangle/token/controller/TokenApiController.java
+++ src/main/java/com/bbangle/bbangle/token/controller/TokenApiController.java
@@ -3,7 +3,8 @@ package com.bbangle.bbangle.token.controller;
 import com.bbangle.bbangle.common.dto.CommonResult;
 import com.bbangle.bbangle.common.dto.SingleResult;
 import com.bbangle.bbangle.common.service.ResponseService;
-import com.bbangle.bbangle.token.jwt.TokenCookieProvider;
+import com.bbangle.bbangle.token.dto.CreateAccessTokenRequest;
+import com.bbangle.bbangle.token.dto.CreateAccessTokenResponse;
 import com.bbangle.bbangle.token.jwt.TokenProvider;
 import com.bbangle.bbangle.token.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,22 +22,20 @@ public class TokenApiController {
     private final TokenService tokenService;
     private final ResponseService responseService;
     private final TokenProvider tokenProvider;
-    private final TokenCookieProvider tokenCookieProvider;
 
     @PostMapping("/api/v1/token")
     @Operation(summary = "새로운 엑세스 토큰 발급", description ="쿠키로 엑세스 토큰 받고 쿠키로 전달 / 쿠키 헤더: Set-Cookie, 쿠키명: access_token")
     public CommonResult createNewAccessToken(
-            HttpServletRequest request,
-            HttpServletResponse response
+            @RequestBody
+            CreateAccessTokenRequest request
     ) {
-        String refreshToken = tokenCookieProvider.getRefreshTokenFromCookie(request);
-        if (refreshToken == null || !tokenProvider.isValidToken(refreshToken)) {
+        if (!tokenProvider.isValidToken(request.getRefreshToken())) {
             return responseService.getSuccessResult("Unexpected token", 0);
         }
 
-        String newAccessToken = tokenService.createNewAccessToken(refreshToken);
-        tokenCookieProvider.addAccessTokenCookie(response, newAccessToken);
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
 
-        return responseService.getSuccessResult("Token refreshed successfully", 1);
+        return responseService.getSingleResult(new CreateAccessTokenResponse(newAccessToken));
     }
+
 }
