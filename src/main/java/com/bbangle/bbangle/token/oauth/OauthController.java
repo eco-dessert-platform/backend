edacf8ca76f5d1b26a13273b65854325d2b51b0a
sunwon <92251131+sunwon12@users.noreply.github.com>
2025-07-04 22:00:22 +0900

    [Refactor] JWT로 롤백 (#407)

--- src/main/java/com/bbangle/bbangle/token/oauth/OauthController.java
+++ src/main/java/com/bbangle/bbangle/token/oauth/OauthController.java
@@ -2,11 +2,9 @@ package com.bbangle.bbangle.token.oauth;
 
 import com.bbangle.bbangle.common.dto.CommonResult;
 import com.bbangle.bbangle.common.service.ResponseService;
-import com.bbangle.bbangle.token.jwt.TokenCookieProvider;
 import com.bbangle.bbangle.token.oauth.domain.OauthServerType;
 import com.bbangle.bbangle.token.oauth.infra.kakao.dto.LoginTokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,20 +19,16 @@ public class OauthController {
 
     private final OauthService oauthService;
     private final ResponseService responseService;
-    private final TokenCookieProvider tokenCookieProvider;
 
     @GetMapping("/login/{oauthServerType}")
     @Operation(summary = "Oauth 로그인")
     CommonResult login(
-        @PathVariable("oauthServerType") OauthServerType oauthServerType,
-        @RequestParam String tokenByOauth,
-        HttpServletResponse response
+            @PathVariable("oauthServerType")
+            OauthServerType oauthServerType,
+            @RequestParam("token")
+            String token
     ) {
-        LoginTokenResponse loginTokenResponse = oauthService.login(oauthServerType, tokenByOauth);
-        
-        tokenCookieProvider.addAccessTokenCookie(response, loginTokenResponse.accessToken());
-        tokenCookieProvider.addRefreshTokenCookie(response, loginTokenResponse.refreshToken());
-
-        return responseService.getSuccessResult("Login successful", 1);
+        LoginTokenResponse loginTokenResponse = oauthService.login(oauthServerType, token);
+        return responseService.getSingleResult(loginTokenResponse);
     }
 }
