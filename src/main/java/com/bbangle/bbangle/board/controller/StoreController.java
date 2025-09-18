ac010623d0f5a25555f27e4099379073bc71f997
kmindev <kangkm0324@gmail.com>
2025-09-18 15:24:36 +0900

    [Feat] 스토어 검색 API 설계 및 문서화
    - 요청 파라미터명을 searchValue로 지정하여 추후 확장성 고려
      (예: 가게명 검색 외 제품 기반 스토어 검색 등)

--- src/main/java/com/bbangle/bbangle/board/controller/StoreController.java
+++ src/main/java/com/bbangle/bbangle/board/controller/StoreController.java
@@ -1,6 +1,8 @@
 package com.bbangle.bbangle.board.controller;
 
-import com.bbangle.bbangle.board.controller.dto.StoreResponse;
+import static com.bbangle.bbangle.board.controller.dto.StoreResponse.StoreDetail;
+
+import com.bbangle.bbangle.board.controller.dto.StoreResponse.SearchResponse;
 import com.bbangle.bbangle.board.controller.mapper.StoreMapper;
 import com.bbangle.bbangle.board.facade.StoreFacade;
 import com.bbangle.bbangle.board.service.dto.StoreInfo;
@@ -9,6 +11,10 @@ import com.bbangle.bbangle.common.dto.ListResult;
 import com.bbangle.bbangle.common.dto.SingleResult;
 import com.bbangle.bbangle.common.page.CursorPageResponse;
 import com.bbangle.bbangle.common.service.ResponseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Store", description = "스토어 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/stores")
@@ -28,41 +35,55 @@ public class StoreController {
     private final ResponseService responseService;
 
     @GetMapping("/{storeId}")
-    public SingleResult<StoreResponse.StoreDetail> getPopularBoardResponse(
-            @PathVariable("storeId")
-            Long storeId,
-            @AuthenticationPrincipal
-            Long memberId
+    public SingleResult<StoreDetail> getPopularBoardResponse(
+        @PathVariable("storeId")
+        Long storeId,
+        @AuthenticationPrincipal
+        Long memberId
     ) {
         StoreInfo.StoreDetail storeDetail = storeFacade.getStoreDetail(memberId, storeId);
-        StoreResponse.StoreDetail storeDetailResponse = storeMapper.toStoreDetailResponse(
-                storeDetail);
+        StoreDetail storeDetailResponse = storeMapper.toStoreDetailResponse(
+            storeDetail);
         return responseService.getSingleResult(storeDetailResponse);
     }
 
     @GetMapping("/{storeId}/boards/best")
     public ListResult<StoreInfo.BestBoard> getPopularBoardResponses(
-            @PathVariable("storeId")
-            Long storeId,
-            @AuthenticationPrincipal
-            Long memberId
+        @PathVariable("storeId")
+        Long storeId,
+        @AuthenticationPrincipal
+        Long memberId
     ) {
         List<StoreInfo.BestBoard> popularBoardResponses = storeFacade.getBestBoards(
-                memberId, storeId);
+            memberId, storeId);
         return responseService.getListResult(popularBoardResponses);
     }
 
     @GetMapping("/{storeId}/boards")
     public SingleResult<CursorPageResponse<AllBoard>> getStoreAllBoard(
-            @PathVariable("storeId")
-            Long storeId,
-            @RequestParam(value = "cursorId", required = false)
-            Long boardIdAsCursorId,
-            @AuthenticationPrincipal
-            Long memberId
+        @PathVariable("storeId")
+        Long storeId,
+        @RequestParam(value = "cursorId", required = false)
+        Long boardIdAsCursorId,
+        @AuthenticationPrincipal
+        Long memberId
     ) {
         CursorPageResponse<AllBoard> response = storeFacade.getAllBoard(memberId, storeId, boardIdAsCursorId);
         return responseService.getSingleResult(response);
-
     }
+
+    @Operation(summary = "스토어 검색")
+    @GetMapping("/search")
+    public ListResult<SearchResponse> search(
+        @RequestParam
+        @Parameter(description = "검색어", example = "빵그리의 오븐")
+        String searchValue
+    ) {
+        // TODO: 구현 필요
+        List<SearchResponse> response = new ArrayList<>();
+        response.add(new SearchResponse(1L, "빵그리의 오븐 즉석빵 상점"));
+        response.add(new SearchResponse(2L, "빵그리의 오븐 공장빵 상점"));
+        return responseService.getListResult(response);
+    }
+
 }
