02dfa304ea495ed0a35ad1f18d69051b88cde97f
kmindev <kangkm0324@gmail.com>
2025-09-18 17:46:44 +0900

    [Feat] 조회날짜 형식 변경
    - LocalDateTime => LocalDate 형식으로 변경

--- src/main/java/com/bbangle/bbangle/order/controller/dto/request/CompletedOrderFilter.java
+++ src/main/java/com/bbangle/bbangle/order/controller/dto/request/CompletedOrderFilter.java
@@ -2,11 +2,11 @@ package com.bbangle.bbangle.order.controller.dto.request;
 
 import com.bbangle.bbangle.order.controller.dto.CompletedOrderStatus;
 import io.swagger.v3.oas.annotations.Parameter;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public record CompletedOrderFilter(
-    @Parameter(description = "조회 시작일", example = "2024-01-01T00:00:00") LocalDateTime startDateTime,
-    @Parameter(description = "조회 종료일", example = "2024-01-31T23:59:59") LocalDateTime endDateTime,
+    @Parameter(description = "조회 시작일", example = "2024-01-01") LocalDate startDate,
+    @Parameter(description = "조회 종료일", example = "2024-01-31") LocalDate endDate,
     @Parameter(description = "완료주문 상태") CompletedOrderStatus status
 ) {
 }
