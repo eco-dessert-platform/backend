4945cacb25c3aa0a84dc54ef3e4aef5ccc145542
kmindev <kangkm0324@gmail.com>
2025-09-18 16:01:41 +0900

    [Feat] 판매자 서류 등록 API 설계 및 문서화
    - Content-Type: multipart/form-data 요청 처리
    - 문서 저장 시 판매자 ID는 memberId를 통해 찾는다.
    - Request: businessLicense(사업자 등록증), mailOrderLicense(통신판매업 신고증), bankbookCopy(개인명의통장사본), foodManufactureLicense(즉석식품가공업&식품제조업)
    - Response: { success, code, message } 공통 응답 구조 적용
    - Swagger 문서화 어노테이션 적용

---
+++ src/main/java/com/bbangle/bbangle/seller/controller/SellerController.java
@@ -0,0 +1,34 @@
+package com.bbangle.bbangle.seller.controller;
+
+import com.bbangle.bbangle.common.dto.CommonResult;
+import com.bbangle.bbangle.common.service.ResponseService;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerDocumentsRegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Seller", description = "판매자 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sellers")
+public class SellerController {
+
+    private final ResponseService responseService;
+
+    @Operation(summary = "판매자 서류 등록")
+    @PostMapping(value = "/documents", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public CommonResult registerDocuments(
+        @ModelAttribute SellerDocumentsRegisterRequest request,
+        @AuthenticationPrincipal Long memberId
+    ) {
+        // TODO: 구현 필요
+        return responseService.getSuccessResult();
+    }
+
+}
