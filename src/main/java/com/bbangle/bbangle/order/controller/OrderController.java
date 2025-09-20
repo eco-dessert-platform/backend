2212c647dda5a1d8a8e82c63128a6eac2e95cc29
kmindev <kangkm0324@gmail.com>
2025-09-20 14:53:34 +0900

    [Feat] 완료 주문 페이징 조회 API 응답 규격 수정
    - Order(1) - OrderItem(N) 구조로 수정
    - 하나의 Order에 N개의 OrderItem 구조이기 때문에 수정함

--- src/main/java/com/bbangle/bbangle/order/controller/OrderController.java
+++ src/main/java/com/bbangle/bbangle/order/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.bbangle.bbangle.order.controller;
 import static com.bbangle.bbangle.order.controller.dto.CompletedOrderStatus.CANCELED;
 import static com.bbangle.bbangle.order.controller.dto.CompletedOrderStatus.PURCHASED;
 import static com.bbangle.bbangle.order.controller.dto.DayOfWeek.MONDAY;
-import static com.bbangle.bbangle.order.controller.dto.DayOfWeek.TUESDAY;
 
 import com.bbangle.bbangle.common.dto.SingleResult;
 import com.bbangle.bbangle.common.page.BbanglePageResponse;
@@ -43,12 +42,13 @@ public class OrderController {
         Long memberId
     ) {
         // TODO: 구현 필요
-        List<OrderSummary> orderSummaries = List.of(
-            new OrderSummary(1L, PURCHASED, "0000-1231", "CJ대한통운", "123-123",
-                LocalDateTime.of(2024, 1, 1, 12, 0), MONDAY, "홍길동", "저칼로리 베이글", 5),
-            new OrderSummary(2L, CANCELED, "0000-1234", "롯데택배", "123-159",
-                LocalDateTime.of(2024, 1, 1, 12, 0), TUESDAY, "김영희", "저당 초콜릿", 10)
-        );
+        OrderSummary.OrderItem orderItem1 = OrderSummary.OrderItem.of(1L, PURCHASED, "CJ대한통운",
+            "123-123", "저칼로리 베이글", 5);
+        OrderSummary.OrderItem orderItem2 = OrderSummary.OrderItem.of(2L, CANCELED, "롯데택배",
+            "123-456", "저당 초콜릿", 10);
+        OrderSummary orderSummary = OrderSummary.of(1L, "000-123", LocalDateTime.of(2024, 1, 1, 12, 0),
+            MONDAY, "홍길동", List.of(orderItem1, orderItem2));
+        List<OrderSummary> orderSummaries = List.of(orderSummary);
         PageImpl<OrderSummary> page = new PageImpl<>(orderSummaries, pageable, 10);
         BbanglePageResponse<OrderSummary> response = BbanglePageResponse.of(page);
         return responseService.getSingleResult(response);
