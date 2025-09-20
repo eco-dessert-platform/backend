588945c3d6dd97d353474a35172445c2ee09c07a
Joon Gyu <qkrwnsrb224@gmail.com>
2025-09-20 17:12:07 +0900

    [Feat] 계좌인증 API 문서 작성 v2

---
+++ src/main/java/com/bbangle/bbangle/seller/controller/SellerController.java
@@ -0,0 +1,25 @@
+package com.bbangle.bbangle.seller.controller;
+
+import com.bbangle.bbangle.common.dto.CommonResult;
+import com.bbangle.bbangle.common.service.ResponseService;
+import com.bbangle.bbangle.seller.controller.SellerRequest.SellerCreateRequest;
+import com.bbangle.bbangle.seller.controller.swagger.SellerApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SellerController implements SellerApi {
+
+    private final ResponseService responseService;
+
+    @Override
+    public CommonResult createSeller(SellerCreateRequest request) {
+        return responseService.getSuccessResult();
+    }
+
+    @Override
+    public CommonResult accountVerification(String accountNumber, String sellerName) {
+        return responseService.getSuccessResult();
+    }
+}
