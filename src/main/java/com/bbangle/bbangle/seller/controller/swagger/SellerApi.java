7bf421ec71579ce438ea14609c2123a61ecef243
Joon Gyu <qkrwnsrb224@gmail.com>
2025-10-20 20:33:42 +0900

    [Refactor] seller 도메인 api 통합 진행 seller api 통합 및 이에 따른 변경사항 수정

--- src/main/java/com/bbangle/bbangle/seller/controller/swagger/SellerApi.java
+++ src/main/java/com/bbangle/bbangle/seller/controller/swagger/SellerApi.java
@@ -2,7 +2,11 @@ package com.bbangle.bbangle.seller.controller.swagger;
 
 import com.bbangle.bbangle.common.dto.CommonResult;
 import com.bbangle.bbangle.exception.GlobalControllerAdvice;
-import com.bbangle.bbangle.seller.controller.dto.SellerRequest_v3.SellerCreateRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerAccountUpdateRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerCreateRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerDocumentsRegisterRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerStoreNameUpdateRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -10,11 +14,49 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "Sellers", description = "판매자 관련 API")
-public interface SellerApi_v3 {
+@Tag(name = "Seller", description = "(판매자) 판매자 API")
+public interface SellerApi {
 
+    @Operation(summary = "(판매자) 판매자 서류 등록")
+    CommonResult registerDocuments(
+        SellerDocumentsRegisterRequest request,
+        Long memberId
+    );
+
+    // TODO: v2
+    @Operation(
+        summary = "판매자 정보 수정",
+        description = "기존 판매자 정보를 전체 수정합니다."
+    )
+    CommonResult updateSeller(
+        @RequestBody SellerUpdateRequest request,
+        @AuthenticationPrincipal Long sellerId
+    );
+
+    @Operation(
+        summary = "스토어명 변경",
+        description = "스토어명을 변경합니다. (최초 1회만 가능)"
+    )
+    CommonResult updateStoreName(
+        @RequestBody SellerStoreNameUpdateRequest request,
+        @AuthenticationPrincipal Long sellerId
+    );
+
+    @Operation(
+        summary = "계좌 정보 변경",
+        description = "판매자 계좌 정보를 변경합니다."
+    )
+    CommonResult updateAccount(
+        @RequestBody SellerAccountUpdateRequest request,
+        @AuthenticationPrincipal Long sellerId
+    );
+
+
+    // TODO : v3
     @Operation(
         summary = "신규 판매자 생성",
         description = "판매자 정보(JSON)와 프로필 이미지 파일을 업로드합니다."
@@ -86,6 +128,5 @@ public interface SellerApi_v3 {
     CommonResult accountVerification(
         String accountNumber,
         String sellerName);
-
 
 }
---
+++ src/main/java/com/bbangle/bbangle/seller/service/SellerService.java
@@ -0,0 +1,28 @@
+package com.bbangle.bbangle.seller.service;
+
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerAccountUpdateRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerStoreNameUpdateRequest;
+import com.bbangle.bbangle.seller.controller.dto.SellerRequest.SellerUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SellerService {
+
+    public void updateSeller(SellerUpdateRequest request, Long sellerId) {
+        // TODO: 실제 비즈니스 로직 구현
+    }
+
+    public void updateStoreName(SellerStoreNameUpdateRequest request, Long sellerId) {
+
+        // TODO: 실제 비즈니스 로직 구현
+
+    }
+
+    public void updateAccount(SellerAccountUpdateRequest request, Long sellerId) {
+
+        // TODO: 실제 비즈니스 로직 구현
+
+    }
+}
