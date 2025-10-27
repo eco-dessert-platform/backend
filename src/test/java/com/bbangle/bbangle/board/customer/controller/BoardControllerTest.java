b259a91a01daa03a9d0bbda93bd672b83f9cf7ad
Joon Gyu <qkrwnsrb224@gmail.com>
2025-10-27 19:18:33 +0900

    [Refactor] test 패키지 구조 리팩터링

---
+++ src/test/java/com/bbangle/bbangle/board/customer/controller/BoardControllerTest.java
@@ -0,0 +1,230 @@
+package com.bbangle.bbangle.board.customer.controller;
+
+import static java.util.Collections.emptyMap;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.bbangle.bbangle.AbstractIntegrationTest;
+import com.bbangle.bbangle.board.domain.Board;
+import com.bbangle.bbangle.board.domain.Category;
+import com.bbangle.bbangle.board.domain.Store;
+import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.member.domain.Role;
+import com.bbangle.bbangle.token.jwt.TokenProvider;
+import com.bbangle.bbangle.wishlist.customer.dto.WishListBoardRequest;
+import com.bbangle.bbangle.wishlist.domain.WishListFolder;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Transactional
+class CustomerBoardControllerTest extends AbstractIntegrationTest {
+
+    @Autowired
+    TokenProvider tokenProvider;
+
+    Board board;
+    Board board2;
+
+    @Nested
+    @DisplayName("기본 게시글 조회 API 테스트")
+    class BasicBoard {
+
+        @BeforeEach
+        void setup() {
+            Store store = storeRepository.save(fixtureStore(emptyMap()));
+
+            Map<String, Object> boardParams = new HashMap<>();
+            boardParams.put("store", store);
+            board = boardRepository.save(fixtureBoard(boardParams));
+            board2 = boardRepository.save(fixtureBoard(boardParams));
+        }
+
+        @Test
+        @DisplayName("순서나 필터링 조건이 없어도 정상적으로 조회한다.")
+        void getBoardListSuccessWithoutAnyCondition() throws Exception {
+            mockMvc.perform(get("/api/v1/boards"))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+
+        @ParameterizedTest
+        @ValueSource(
+            strings = {
+                "glutenFreeTag", "highProteinTag", "sugarFreeTag", "veganTag", "ketogenicTag"
+            }
+        )
+        @DisplayName("순서가 없고 필터링 조건이 있어도 정상적으로 조회한다.")
+        void getBoardListSuccessWithIngredientFilteringCondition(String ingredient)
+            throws Exception {
+            mockMvc.perform(get("/api/v1/boards")
+                    .param(ingredient, "true"))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+
+        @ParameterizedTest
+        @EnumSource(value = Category.class)
+        @DisplayName("순서가 없고 카테고리 필터링 조건이 있어도 정상적으로 조회한다.")
+        void getBoardListSuccessWithCategoryCondition(Category category) throws Exception {
+            mockMvc.perform(get("/api/v1/boards")
+                    .param("category", category.name()))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+
+        @ParameterizedTest
+        @EnumSource(Category.class)
+        @DisplayName("순서가 없고 필터링 조건 둘 이상 있어도 정상적으로 조회한다.")
+        void getBoardListSuccessWithCategoryAndIngredientCondition(Category category)
+            throws Exception {
+            // given
+            MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
+            info.add("ketogenicTag", "true");
+            info.add("category", category.name());
+            // TODO: 이 부분 sort가 추가되면 500 error가 발생하는 문제
+            // info.add("sort", "POPULAR");
+
+            // when, then
+            mockMvc.perform(get("/api/v1/boards")
+                    .params(info))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"bread", "school", "SOCCER", "잼"})
+        @DisplayName("잘못된 카테고리로 카테고리 필터링 검색을 하면 조회한다.")
+        void getBoardListFailWithWrongCategory(String category) throws Exception {
+            mockMvc.perform(get("/api/v1/boards")
+                    .param("category", category))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+        }
+
+        @Nested
+        @DisplayName("getProduct 메서드는")
+        class GetProduct {
+
+            @Test
+            @DisplayName("유효한 boardId로 상품 정보를 가져올 수 있다")
+            void getProductInfo() throws Exception {
+                Long boardId = board.getId();
+                mockMvc.perform(get("/api/v1/boards/" + boardId + "/product"))
+                    .andExpect(status().isOk())
+                    .andDo(print());
+            }
+
+            @Test
+            @DisplayName("유효하지 않은 boardId를 요청 시 400에 에러를 발생시킨다")
+            void throwError() throws Exception {
+                mockMvc.perform(get("/api/v1/boards/9999/product"))
+                    .andExpect(status().is4xxClientError())
+                    .andDo(print());
+            }
+        }
+
+        @Test
+        @DisplayName("")
+        void getProductTest() throws Exception {
+            Long boardId = board.getId();
+            mockMvc.perform(get("/api/v1/boards/" + boardId + "/product"))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+    }
+
+    @Nested
+    @DisplayName("폴더 내 게시글을 조회하는 API test")
+    class BoardInFolderApiTest {
+
+        Long memberId;
+        Role role;
+        Board createdBoard;
+        Long wishListFolderId;
+
+        @BeforeEach
+        void setup() {
+            // Member 생성
+            Member member = memberRepository.save(fixtureMember(emptyMap()));
+            memberId = member.getId();
+            role = member.getRole();
+            List<WishListFolder> wishListFolders = member.getWishListFolders();
+            wishListFolderId = wishListFolders.get(0).getId();
+
+            // Store 생성
+            Store store = storeRepository.save(fixtureStore(emptyMap()));
+
+            // Board 생성
+            Map<String, Object> boardParams = new HashMap<>();
+            boardParams.put("store", store);
+            createdBoard = boardRepository.save(fixtureBoard(boardParams));
+
+            wishListBoardService.wish(member.getId(), createdBoard.getId(),
+                new WishListBoardRequest(wishListFolderId));
+        }
+
+        @Test
+        @DisplayName("정상적으로 폴더 안의 게시글을 조회할 수 있다.")
+        @Transactional
+        void getBoardInFolder() throws Exception {
+            //given
+            String authentication = getAuthentication(memberId, role);
+
+            mockMvc.perform(get("/api/v1/boards/folders/" + wishListFolderId)
+                    .header(HttpHeaders.AUTHORIZATION, authentication))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.code").value(0))
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andExpect(jsonPath("$.result.content[0].boardId").value(createdBoard.getId()))
+                .andExpect(
+                    jsonPath("$.result.content[0].storeId").value(createdBoard.getStore().getId()))
+                .andExpect(jsonPath("$.result.content[0].storeName").value(
+                    createdBoard.getStore().getName()))
+                .andExpect(
+                    jsonPath("$.result.content[0].thumbnail").value(createdBoard.getThumbnail()))
+                .andExpect(jsonPath("$.result.content[0].title").value(createdBoard.getTitle()))
+                .andExpect(jsonPath("$.result.content[0].price").value(createdBoard.getPrice()))
+                .andExpect(jsonPath("$.result.content[0].isWished").value(true))
+                .andExpect(
+                    jsonPath("$.result.content[0].isBundled").value(createdBoard.isBundled()))
+                .andExpect(jsonPath("$.result.content[0].reviewRate").value(
+                    createdBoard.getBoardStatistic().getBoardReviewGrade()
+                        .round(new MathContext(2, RoundingMode.HALF_UP)).doubleValue()))
+                .andExpect(jsonPath("$.result.content[0].reviewCount").value(
+                    createdBoard.getBoardStatistic().getBoardReviewCount()))
+                .andExpect(jsonPath("$.result.content[0].isBbangcketing").value(
+                    createdBoard.isBbangketing()))
+                .andExpect(
+                    jsonPath("$.result.content[0].isSoldOut").value(createdBoard.isSoldOut()))
+                .andExpect(jsonPath("$.result.content[0].discountRate").value(
+                    createdBoard.getDiscountRate()))
+                .andExpect(jsonPath("$.result.nextCursor").value(-1))
+                .andExpect(jsonPath("$.result.hasNext").value(false));
+        }
+
+        private String getAuthentication(Long memberId, Role role) {
+            String token = tokenProvider.generateToken(memberId, role, Duration.ofMinutes(1));
+            return "Bearer " + token;
+        }
+    }
+}
---
+++ src/test/java/com/bbangle/bbangle/review/customer/controller/ReviewControllerTest.java
@@ -0,0 +1,183 @@
+package com.bbangle.bbangle.review.customer.controller;
+
+import static java.util.Collections.emptyMap;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.bbangle.bbangle.AbstractIntegrationTest;
+import com.bbangle.bbangle.board.domain.Board;
+import com.bbangle.bbangle.fixture.MemberFixture;
+import com.bbangle.bbangle.image.domain.Image;
+import com.bbangle.bbangle.image.domain.ImageCategory;
+import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.mock.WithCustomMockUser;
+import com.bbangle.bbangle.review.domain.Badge;
+import com.bbangle.bbangle.review.domain.Review;
+import com.bbangle.bbangle.review.domain.ReviewLike;
+import com.bbangle.bbangle.review.repository.ReviewLikeRepository;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class ReviewControllerTest extends AbstractIntegrationTest {
+
+    private static final String TASTE_REACTION = "맛있어요";
+    private static final String BASE_PATH = "/api/v1/review/";
+    @Autowired
+    private ReviewLikeRepository reviewLikeRepository;
+
+    @BeforeEach
+    public void setUp() {
+        Board board = boardRepository.save(fixtureBoard(emptyMap()));
+        Long boardId = board.getId();
+        Member kakaoMember = MemberFixture.createKakaoMember();
+        Member savedMember = memberRepository.save(kakaoMember);
+        Long memberId = savedMember.getId();
+
+        Review review = Review.builder()
+            .boardId(boardId)
+            .badgeBrix(Badge.SWEET)
+            .badgeTaste(Badge.GOOD)
+            .badgeTexture(Badge.DRY)
+            .content(TASTE_REACTION)
+            .rate(new BigDecimal("4.5"))
+            .memberId(memberId)
+            .build();
+        Review savedReview = reviewRepository.save(review);
+
+        ReviewLike reviewLike = ReviewLike.builder()
+            .reviewId(savedReview.getId())
+            .memberId(memberId)
+            .build();
+        reviewLikeRepository.save(reviewLike);
+
+        Image reviewImg = Image.builder()
+            .path("testImage")
+            .domainId(savedReview.getId())
+            .imageCategory(ImageCategory.REVIEW)
+            .build();
+
+        imageRepository.save(reviewImg);
+    }
+
+    @Test
+    @DisplayName("도움돼요, 리뷰 이미지, 리뷰를 삭제한다")
+    @WithCustomMockUser
+    void deleteReview() throws Exception {
+        Long reviewId = reviewRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(delete(BASE_PATH + reviewId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물에 있는 리뷰의 평점과 개수를 조회한다")
+    void getReviewRate() throws Exception {
+        Long boardId = boardRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(get("/api/v1/review/rate/" + boardId))
+            .andExpect(jsonPath("$.result.rating").value("4.5"))
+            .andExpect(jsonPath("$.result.count").value(1))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물에 있는 리뷰 목록을 조회한다")
+    void getReviews() throws Exception {
+        Long boardId = boardRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(get("/api/v1/review/list/" + boardId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("도움돼요를 추가한다")
+    @WithCustomMockUser
+    void insertLike() throws Exception {
+        reviewLikeRepository.deleteAll();
+        Long reviewId = reviewRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(post("/api/v1/review/like/" + reviewId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("도움돼요를 삭제한다")
+    @WithCustomMockUser
+    void removeLike() throws Exception {
+        Long reviewId = reviewRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(delete("/api/v1/review/like/" + reviewId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("리뷰 상세 정보를 조회한다")
+    @WithCustomMockUser
+    void getReviewDetail() throws Exception {
+        Long reviewId = reviewRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(get(BASE_PATH + reviewId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("리뷰 대표 이미지를 조회한다")
+    void getReviewImages() throws Exception {
+        Long reviewId = reviewRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(get(BASE_PATH + "/image/" + reviewId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("내가 작성한 리뷰를 조회한다")
+    @WithCustomMockUser
+    void getMyReviews() throws Exception {
+        mockMvc.perform(get(BASE_PATH + "/myreview"))
+            .andExpect(status().isOk())
+            .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("리뷰의 전체 사진을 조회한다")
+    void getAllImagesByBoardId() throws Exception {
+        Long boardId = boardRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(get(BASE_PATH + boardId + "/images"))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("리뷰의 사진을 크게 본다")
+    void getImage() throws Exception {
+        Long reviewImgId = imageRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(get(BASE_PATH + "/images/" + reviewImgId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("리뷰의 사진을 삭제한다")
+    @WithCustomMockUser
+    void deleteImage() throws Exception {
+        Long reviewImgId = imageRepository.findAll().stream().findFirst().get().getId();
+        mockMvc.perform(delete(BASE_PATH + "/image/" + reviewImgId))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+}
