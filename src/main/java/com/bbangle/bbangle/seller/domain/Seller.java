38bb8d4b40d3b841850c8f86070287c9f1d3532a
kmindev <kangkm0324@gmail.com>
2025-11-04 10:57:02 +0900

    [Feat] Seller 엔티티 추가 및 연관관계 설정
    - 연관관계 매핑만 맺고, 추후 상세 필드 정의

---
+++ src/main/java/com/bbangle/bbangle/seller/domain/Seller.java
@@ -0,0 +1,31 @@
+package com.bbangle.bbangle.seller.domain;
+
+import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.store.domain.Store;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Table(name = "sellers")
+@Entity
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+}
