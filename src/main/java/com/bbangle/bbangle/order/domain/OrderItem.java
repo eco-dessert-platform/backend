cca17e206ddcfa3674d0a30a137809535d0f1f92
kmindev <kangkm0324@gmail.com>
2025-11-04 11:15:20 +0900

    [Feat] OrderItem 엔테티 추가 및 연관관계 설정
    - 연관관계 매핑만 맺고, 추후 상세 필드 정의

---
+++ src/main/java/com/bbangle/bbangle/order/domain/OrderItem.java
@@ -0,0 +1,34 @@
+package com.bbangle.bbangle.order.domain;
+
+import com.bbangle.bbangle.board.domain.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "order_item")
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
