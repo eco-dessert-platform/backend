2f50d0ef01b6f9d84b34fb39248b33afd08d5646
kmindev <kangkm0324@gmail.com>
2025-11-04 13:11:13 +0900

    [Feat] HealthTag, ProductHealthTag 엔티티 추가 및 연관관계 설정
    - HealthTag 엔티티 추가
    - Product - HealthTag  매핑 엔티티 추가
    - 연관관계 매핑만 맺고, 추후 상세 필드 정의

---
+++ src/main/java/com/bbangle/bbangle/board/domain/HealthTag.java
@@ -0,0 +1,22 @@
+package com.bbangle.bbangle.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "health_tag")
+@Entity
+public class HealthTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ src/main/java/com/bbangle/bbangle/board/domain/ProductHealthTag.java
@@ -0,0 +1,33 @@
+package com.bbangle.bbangle.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "product_health_tag")
+@Entity
+public class ProductHealthTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "health_tag_id")
+    private HealthTag healthTag;
+
+}
