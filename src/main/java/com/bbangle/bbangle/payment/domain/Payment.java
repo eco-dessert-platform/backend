9b99baa170c80bfd31f30ecba7761c19a3f08285
kmindev <kangkm0324@gmail.com>
2025-11-04 11:28:39 +0900

    [Feat] Payment 엔티티 추가 및 연관관계 설정
    - 연관관계 매핑만 맺고, 추후 상세 필드 정의

---
+++ src/main/java/com/bbangle/bbangle/payment/domain/Payment.java
@@ -0,0 +1,31 @@
+package com.bbangle.bbangle.payment.domain;
+
+import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.order.domain.Order;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "payment")
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}
