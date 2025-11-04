eccf618cdb3b4034abe6a4ab32092056529f32f0
kmindev <kangkm0324@gmail.com>
2025-11-04 13:00:15 +0900

    [Feat] ShippingPolicy 엔티티 추가 및 연관관계 설정
    - 연관관계 매핑만 맺고, 추후 상세 필드 정의

---
+++ src/main/java/com/bbangle/bbangle/board/domain/ShippingPolicy.java
@@ -0,0 +1,33 @@
+package com.bbangle.bbangle.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "shipping_policy")
+@Entity
+public class ShippingPolicy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "courier_id")
+    private Courier courier;
+
+}
