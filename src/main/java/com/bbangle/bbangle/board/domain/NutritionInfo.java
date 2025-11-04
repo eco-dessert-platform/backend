3b08fe9dd977f72116f8b515dabbcf7219d28f7f
kmindev <kangkm0324@gmail.com>
2025-11-04 13:15:05 +0900

    [Feat] ProductHealthTag 엔티티 추가 및 연관관계 설정
    - 연관관계 매핑만 맺고, 추후 상세 필드 정의

---
+++ src/main/java/com/bbangle/bbangle/board/domain/NutritionInfo.java
@@ -0,0 +1,29 @@
+package com.bbangle.bbangle.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "nutrition_info")
+@Entity
+public class NutritionInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
