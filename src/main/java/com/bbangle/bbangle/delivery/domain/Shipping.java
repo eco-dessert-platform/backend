0370553ee982f55b2f8c69b9b5135010fc3bf46b
Joon Gyu <qkrwnsrb224@gmail.com>
2025-11-05 14:19:50 +0900

    [Feat] Shipping 클래스 개선 1. ShippingStatus 부분을 제외한 나머지를 ShippingInfo로 추출함으로써 좀    더 범용성 있게 활용하도록 개선

--- src/main/java/com/bbangle/bbangle/delivery/domain/Shipping.java
+++ src/main/java/com/bbangle/bbangle/delivery/domain/Shipping.java
@@ -2,9 +2,9 @@ package com.bbangle.bbangle.delivery.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,22 +14,13 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class Shipping {
 
-    @Column(length = 255)
-    private String deliveryMemo;
-
-    @Column(length = 50)
-    private String courierName;
-
-    @Column(length = 50)
-    private String trackingNumber;
-
-    private Integer fee;
+    @Embedded
+    private ShippingInfo shippingInfo;
 
     @Column(name = "status", length = 30, columnDefinition = "varchar(30)")
     @Enumerated(EnumType.STRING)
     private ShippingStatus status;
 
-    private LocalDateTime shippedAt;
-    private LocalDateTime deliveredAt;
+
 
 }
