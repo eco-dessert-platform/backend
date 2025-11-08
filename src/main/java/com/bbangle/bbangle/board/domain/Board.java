e39512480ab93f8bc4d5f5ad6e150aff2f3b5de1
HyunJun-git <chj790700@gmail.com>
2025-11-08 23:20:22 +0900

    [Feat] Board 쪽 배송 쪽 원복

--- src/main/java/com/bbangle/bbangle/board/domain/Board.java
+++ src/main/java/com/bbangle/bbangle/board/domain/Board.java
@@ -44,6 +44,14 @@ public class Board extends BaseEntity {
     @JoinColumn(name = "store_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Store store;
 
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_info_notice_id")
+    private ProductInfoNotice productInfoNotice;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_detail_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private BoardDetail boardDetail;
+
     @Column(name = "title")
     private String title;
 
@@ -68,19 +76,20 @@ public class Board extends BaseEntity {
     @Column(name = "is_deleted", columnDefinition = "tinyint")
     private boolean isDeleted;
 
+    @Column(name = "view")
+    private int view;
+
+    @Column(name = "wish_cnt")
+    private int wishCnt;
+
+    @Column(name = "discount_price")
+    private int discountPrice;
+
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
     private List<Product> products = new ArrayList<>();
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
     private List<ProductImg> productImgs = new ArrayList<>();
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "board_detail_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private BoardDetail boardDetail;
-
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "product_info_notice_id")
-    private ProductInfoNotice productInfoNotice;
 
     @OneToOne(mappedBy = "board", cascade = CascadeType.ALL)
     // Board가 더 많이 호출되므로 연관관계 주인을 board로 하는게 더 적합해 보임
