60624cd1cb72a832a8172174cc5b5460f0f8c8e8
Joon Gyu <107477191+CUCU7103@users.noreply.github.com>
2025-11-18 21:40:59 +0900

    [Refactor] Seller 도메인 수정 및 Command 객체 수정 1. Seller 객체 생성시 store 포함하여 생성하도록 변경 2. Command 객체에 유효성 검사 추가

--- src/main/java/com/bbangle/bbangle/seller/seller/service/command/SellerCreateCommand.java
+++ src/main/java/com/bbangle/bbangle/seller/seller/service/command/SellerCreateCommand.java
@@ -1,8 +1,9 @@
 package com.bbangle.bbangle.seller.seller.service.command;
 
+import com.bbangle.bbangle.exception.BbangleErrorCode;
+import com.bbangle.bbangle.exception.BbangleException;
 import lombok.Builder;
 
-@Builder
 public record SellerCreateCommand(
         String storeName,
         String phoneNumber,
@@ -11,5 +12,39 @@ public record SellerCreateCommand(
         String originAddress,
         String originAddressDetail
 ) {
+
+    @Builder
+    public SellerCreateCommand(String storeName, String phoneNumber, String subPhoneNumber,
+        String email, String originAddress, String originAddressDetail) {
+        this.storeName = storeName;
+        this.phoneNumber = phoneNumber;
+        this.subPhoneNumber = subPhoneNumber;
+        this.email = email;
+        this.originAddress = originAddress;
+        this.originAddressDetail = originAddressDetail;
+        validate();
+    }
+
+    private void validate() {
+        if(storeName == null || storeName.isEmpty()) {
+            throw new BbangleException(BbangleErrorCode.INVALID_STORE_NAME);
+        }
+        if(phoneNumber == null || phoneNumber.isEmpty()) {
+            throw new BbangleException(BbangleErrorCode.INVALID_PHONE_NUMBER);
+        }
+        if (subPhoneNumber == null || subPhoneNumber.isEmpty()) {
+            throw new BbangleException(BbangleErrorCode.INVALID_PHONE_NUMBER);
+        }
+        if(email == null || email.isEmpty() || !email.contains("@")) {
+            throw new BbangleException(BbangleErrorCode.INVALID_EMAIL);
+        }
+        if(originAddress == null || originAddress.isEmpty()) {
+            throw new BbangleException(BbangleErrorCode.INVALID_ADDRESS);
+        }
+        if(originAddressDetail == null || originAddressDetail.isEmpty()) {
+            throw new BbangleException(BbangleErrorCode.INVALID_DETAIL_ADDRESS);
+        }
+    }
+
 
 }
