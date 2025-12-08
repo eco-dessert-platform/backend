2cd342d0d00c101a5bb45f0ce71335fa9edb36ab
kmindev <kangkm0324@gmail.com>
2025-12-08 23:45:00 +0900

    [Refactor]: flyway sql 파일 통합

--- src/main/resources/flyway/V24__app.sql
+++ src/main/resources/flyway/V24__app.sql
@@ -8,3 +8,14 @@ CREATE TABLE admin (
                        modified_at DATETIME(6) ,
                        PRIMARY KEY (id)
 );
+
+-- Add admin_id column to refresh_token table
+ALTER TABLE refresh_token
+    ADD COLUMN admin_id BIGINT UNIQUE;
+
+-- member id null 값 허용
+ALTER TABLE refresh_token
+    MODIFY member_id BIGINT NULL;
+
+ALTER TABLE member
+DROP COLUMN member_role;
--- src/main/resources/flyway/V25__app.sql
+++
@@ -1,9 +0,0 @@
--- Add admin_id column to refresh_token table
-ALTER TABLE refresh_token
-    ADD COLUMN admin_id BIGINT UNIQUE;
-
--- member id null 값 허용
-ALTER TABLE refresh_token
-    MODIFY member_id BIGINT NULL;
-
-
