7afdef6d00f5cb840e35b6c411da933be114d825
kmindev <kangkm0324@gmail.com>
2025-12-21 15:56:37 +0900

    [fix]: 의존성 순환참조 문제 해결 - RestTemplateConfig 추가

--- src/main/java/com/bbangle/bbangle/config/WebConfig.java
+++ src/main/java/com/bbangle/bbangle/config/WebConfig.java
@@ -1,43 +1,35 @@
 package com.bbangle.bbangle.config;
 
-import java.util.List;
-
 import com.bbangle.bbangle.common.service.RequestTimeInterceptor;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  private final RequestTimeInterceptor requestTimeInterceptor;
-  private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+    private final RequestTimeInterceptor requestTimeInterceptor;
+    private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
 
-  @Autowired
-  public WebConfig(
-      OctetStreamReadMsgConverter octetStreamReadMsgConverter,
-      RequestTimeInterceptor requestTimeInterceptor) {
-    this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
-    this.requestTimeInterceptor = requestTimeInterceptor;
-  }
+    @Autowired
+    public WebConfig(
+        OctetStreamReadMsgConverter octetStreamReadMsgConverter,
+        RequestTimeInterceptor requestTimeInterceptor) {
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+        this.requestTimeInterceptor = requestTimeInterceptor;
+    }
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(octetStreamReadMsgConverter);
+    }
 
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    converters.add(octetStreamReadMsgConverter);
-  }
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(requestTimeInterceptor);
-  }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestTimeInterceptor);
+    }
 
 }
