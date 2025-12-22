9807d43bfa44d229d8bd140ca7e8b2afc2d8f134
Joon Gyu <qkrwnsrb224@gmail.com>
2025-12-23 00:30:36 +0900

    [Fix] merge 충돌나는 부분 수정

--- src/main/java/com/bbangle/bbangle/config/WebConfig.java
+++ src/main/java/com/bbangle/bbangle/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.bbangle.bbangle.config;
 import com.bbangle.bbangle.common.service.RequestTimeInterceptor;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -20,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     this.requestTimeInterceptor = requestTimeInterceptor;
   }
 
+
   @Override
   public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
     converters.add(octetStreamReadMsgConverter);
@@ -29,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(requestTimeInterceptor)
-        .addPathPatterns("/**")
+        .addPathPatterns("/**") 
         .excludePathPatterns(
             "/swagger-ui/**",
             "/v3/api-docs/**",
