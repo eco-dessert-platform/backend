3538a5befcc54d6578318904c5dc19a9caa4e100
kmindev <kangkm0324@gmail.com>
2026-01-03 17:30:52 +0900

    [Feature] (어드민) 상품게시글 페이징 조회 기능 구현 - board와 boardStatistic(주인)이 1:1 관계에서 board(주인이 아닌쪽)에서 조회를 하면 LAZY 옵션 상관없이 boardStatistic 접근이 없음에도 N 번만큼 boardStatistic 조회쿼리가 발생한다. - EntityGraph를 통해 조인을 활용하여 boardStatistic를 즉시 로딩하도록 구현함
    충돌 해결

--- src/main/java/com/bbangle/bbangle/board/repository/BoardRepository.java
+++ src/main/java/com/bbangle/bbangle/board/repository/BoardRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardQueryDSLRepository {
 
     @EntityGraph(attributePaths = {"store", "boardDetail", "boardStatistic"})
