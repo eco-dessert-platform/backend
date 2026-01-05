0692c273c6c9efc9de2232eecf5d314916f5fd42
kmindev <kangkm0324@gmail.com>
2026-01-05 22:15:14 +0900

    [feat]: (관리자) 상품 리스트 삭제 기능 구현 - 다건 삭제이므로, 더티 체킹 활용하지 않고, 벌크 업데이트 쿼리로 처리

--- src/main/java/com/bbangle/bbangle/board/admin/controller/AdminBoardController.java
+++ src/main/java/com/bbangle/bbangle/board/admin/controller/AdminBoardController.java
@@ -3,17 +3,21 @@ package com.bbangle.bbangle.board.admin.controller;
 import com.bbangle.bbangle.board.admin.controller.dto.AdminProductResponse;
 import com.bbangle.bbangle.board.admin.controller.swagger.AdminBoardApi;
 import com.bbangle.bbangle.board.admin.service.AdminBoardService;
+import com.bbangle.bbangle.common.dto.CommonResult;
 import com.bbangle.bbangle.common.dto.SingleResult;
 import com.bbangle.bbangle.common.page.BbanglePageResponse;
 import com.bbangle.bbangle.common.service.ResponseService;
 import com.bbangle.bbangle.config.security.AdminApiPath;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -33,6 +37,13 @@ public class AdminBoardController implements AdminBoardApi {
     ) {
         Page<AdminProductResponse> result = adminBoardService.getAdminBoards(pageable);
         return responseService.getSingleResult(BbanglePageResponse.of(result));
+    }
+
+    @Override
+    @DeleteMapping
+    public CommonResult deleteBoards(@RequestParam List<Long> productIds) {
+        adminBoardService.deleteBoards(productIds);
+        return responseService.getSuccessResult();
     }
 
 }
--- src/main/java/com/bbangle/bbangle/board/repository/ProductInfoNoticeRepository.java
+++ src/main/java/com/bbangle/bbangle/board/repository/ProductInfoNoticeRepository.java
@@ -1,7 +1,24 @@
 package com.bbangle.bbangle.board.repository;
 
 import com.bbangle.bbangle.board.domain.ProductInfoNotice;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductInfoNoticeRepository extends JpaRepository<ProductInfoNotice, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE ProductInfoNotice pin
+            SET pin.isDeleted = true
+            WHERE pin.id IN (
+                SELECT b.productInfoNotice.id
+                FROM Board b
+                WHERE b.id IN :boardIds
+            )
+        """)
+    void softDeleteByBoardIds(@Param("boardIds") List<Long> boardIds);
+
 }
