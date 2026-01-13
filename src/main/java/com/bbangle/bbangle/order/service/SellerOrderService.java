8c804989e7399cb675f463b509c39b767df25812
yeju <bbogaeme@gmail.com>
2026-01-13 23:16:21 +0900

    [Feat] 판매자 주문 조회 비즈니스 로직 구현

---
+++ src/main/java/com/bbangle/bbangle/order/service/SellerOrderService.java
@@ -0,0 +1,45 @@
+package com.bbangle.bbangle.order.service;
+
+import com.bbangle.bbangle.exception.BbangleErrorCode;
+import com.bbangle.bbangle.exception.BbangleException;
+import com.bbangle.bbangle.order.controller.dto.response.SellerOrderResponse.OrderConfirmResponse;
+import com.bbangle.bbangle.order.domain.Order;
+import com.bbangle.bbangle.order.domain.OrderItem;
+import com.bbangle.bbangle.order.repository.OrderItemRepository;
+import com.bbangle.bbangle.order.repository.OrderRepository;
+import com.bbangle.bbangle.order.service.model.SellerOrderCommand.OrderConfirmCommand;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SellerOrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+
+    @Transactional
+    public OrderConfirmResponse confirmOrder(OrderConfirmCommand command) {
+
+        Order order = orderRepository.findById(command.orderId())
+            .orElseThrow(() -> new BbangleException(BbangleErrorCode.ORDER_NOT_FOUND));
+
+        List<OrderItem> orderItems = orderItemRepository.findByOrderIdAndIdIn(
+            order.getId(),
+            command.orderItemIds()
+        );
+
+        List<Long> confirmedOrderItemIds = new ArrayList<>();
+
+        for (OrderItem orderItem : orderItems) {
+            if (orderItem.confirmOrder()) {
+                confirmedOrderItemIds.add(orderItem.getId());
+            }
+        }
+
+        return OrderConfirmResponse.of(order.getId(), confirmedOrderItemIds);
+    }
+}
