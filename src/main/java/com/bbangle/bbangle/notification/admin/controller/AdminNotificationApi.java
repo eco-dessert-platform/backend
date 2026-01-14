d6c7212a09de190e9f26a741a76d3d6d9cdd582e
Joon Gyu <qkrwnsrb224@gmail.com>
2026-01-15 00:17:20 +0900

    [Feat] (관리자) 공지사항 수정 기능 추가 (#552)
    관리자 공지사항 기능 추가

--- src/main/java/com/bbangle/bbangle/notification/admin/controller/AdminNotificationApi.java
+++ src/main/java/com/bbangle/bbangle/notification/admin/controller/AdminNotificationApi.java
@@ -2,7 +2,9 @@ package com.bbangle.bbangle.notification.admin.controller;
 
 import com.bbangle.bbangle.common.dto.SingleResult;
 import com.bbangle.bbangle.notification.admin.controller.dto.AdminNotificationRequest.AdminNotificationCreateRequest;
+import com.bbangle.bbangle.notification.admin.controller.dto.AdminNotificationRequest.AdminNotificationUpdateRequest;
 import com.bbangle.bbangle.notification.admin.controller.dto.AdminNotificationResponse.AdminNotificationCreateResponse;
+import com.bbangle.bbangle.notification.admin.controller.dto.AdminNotificationResponse.AdminNotificationUpdateResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
@@ -16,6 +18,14 @@ public interface AdminNotificationApi {
     SingleResult<AdminNotificationCreateResponse> registerNotification(
         Long adminId,
         AdminNotificationCreateRequest request,
+        List<MultipartFile> profileImage
+    );
+
+    @Operation(summary = "(관리자) 공지사항 수정")
+    SingleResult<AdminNotificationUpdateResponse> updateNotification(
+        Long adminId,
+        Long noticeId,
+        AdminNotificationUpdateRequest request,
         List<MultipartFile> profileImage
     );
 }
--- src/main/java/com/bbangle/bbangle/notification/admin/controller/dto/AdminNotificationResponse.java
+++ src/main/java/com/bbangle/bbangle/notification/admin/controller/dto/AdminNotificationResponse.java
@@ -37,5 +37,34 @@ public class AdminNotificationResponse {
         }
     }
 
+    @Schema(description = "관리자 공지사항 수정 응답 DTO")
+    @Builder
+    public record AdminNotificationUpdateResponse(
+        @Schema(description = "공지사항 ID")
+        Long id,
+        @Schema(description = "제목")
+        String title,
+        @Schema(description = "내용")
+        String content,
+        @Schema(description = "이미지 링크 목록")
+        List<String> imageLinks,
+        @Schema(description = "생성 일시")
+        LocalDateTime createAt,
+        @Schema(description = "수정 일시")
+        LocalDateTime modifiedAt
+    ){
+
+        public static AdminNotificationUpdateResponse from(NoticeInfo noticeInfo) {
+            return AdminNotificationUpdateResponse.builder()
+                .id(noticeInfo.id())
+                .title(noticeInfo.title())
+                .content(noticeInfo.content())
+                .imageLinks(noticeInfo.imageLinks())
+                .createAt(noticeInfo.createAt())
+                .modifiedAt(noticeInfo.modifiedAt())
+                .build();
+        }
+    }
+
 
 }
