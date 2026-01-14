2871fef474a555807efbf9a3c1b6a439c449336d
kimbro97 <rlagudwog@gmail.com>
2026-01-14 20:19:19 +0900

    [Feature] SellerDocumentRepository 작업

---
+++ src/main/java/com/bbangle/bbangle/seller/repository/SellerDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.bbangle.bbangle.seller.repository;
+
+import com.bbangle.bbangle.seller.domain.SellerDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerDocumentRepository extends JpaRepository<SellerDocument, Long> {
+}
