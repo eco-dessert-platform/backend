4c11c92b4b01c23fc3c4200a82d95389916271f5
Bang <121658176+MSBANG@users.noreply.github.com>
2026-01-18 20:45:01 +0900

    [Feat] 관리자 재고 상태 변경 Controller 추가
    1. Swagger 문서 추가
    2. Controller Service 연동

---
+++ src/main/java/com/bbangle/bbangle/board/admin/controller/AdminProductController.java
@@ -0,0 +1,37 @@
+package com.bbangle.bbangle.board.admin.controller;
+
+import com.bbangle.bbangle.board.admin.controller.dto.AdminEditStockRequest;
+import com.bbangle.bbangle.board.admin.controller.swagger.AdminProductApi;
+import com.bbangle.bbangle.board.admin.service.AdminProductService;
+import com.bbangle.bbangle.common.dto.CommonResult;
+import com.bbangle.bbangle.common.service.ResponseService;
+import com.bbangle.bbangle.config.security.AdminApiPath;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping(AdminApiPath.PREFIX + "/options")
+@RestController
+public class AdminProductController implements AdminProductApi {
+    private final AdminProductService adminProductService;
+    private final ResponseService responseService;
+
+    @Override
+    @PatchMapping("{optionId}/stock")
+    public CommonResult editProductOptionStock(
+        @PathVariable Long optionId,
+        @RequestBody @NotNull AdminEditStockRequest adminEditStockRequest
+    ) {
+        adminProductService.editProductStock(
+            optionId,
+            adminEditStockRequest.amount(),
+            adminEditStockRequest.editStockFlag()
+        );
+        return responseService.getSuccessResult();
+    }
+}
