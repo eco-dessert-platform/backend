1215a776a7b8d217b058062fee73b380d788a8d3
kmindev <kangkm0324@gmail.com>
2026-02-03 22:13:17 +0900

    [Feature] 로컬 환경에서 AuthenticationPrincipal nullPointException 방지를 위해 LocalSecurityArgumentResolver 추가 - mock User(기본값 1)를 반환을 반환하도록 세팅

--- src/main/java/com/bbangle/bbangle/config/WebConfig.java
+++ src/main/java/com/bbangle/bbangle/config/WebConfig.java
@@ -1,44 +1,47 @@
 package com.bbangle.bbangle.config;
+
 import com.bbangle.bbangle.common.service.RequestTimeInterceptor;
+import com.bbangle.bbangle.config.security.resolver.LocalSecurityArgumentResolver;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  private final RequestTimeInterceptor requestTimeInterceptor;
-  private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+    private final RequestTimeInterceptor requestTimeInterceptor;
+    private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+    private final Optional<LocalSecurityArgumentResolver> localSecurityArgumentResolver;
 
-  @Autowired
-  public WebConfig(
-      OctetStreamReadMsgConverter octetStreamReadMsgConverter,
-      RequestTimeInterceptor requestTimeInterceptor) {
-    this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
-    this.requestTimeInterceptor = requestTimeInterceptor;
-  }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(octetStreamReadMsgConverter);
+    }
 
+    // Local 환경에서만 @AuthenticationPrincipal을 위한 ArgumentResolver 등록
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        localSecurityArgumentResolver.ifPresent(resolvers::add);
+    }
 
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    converters.add(octetStreamReadMsgConverter);
-  }
-
-  // 불필요한 swagger 로그 찍히는 부분 설정
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(requestTimeInterceptor)
-        .addPathPatterns("/**") 
-        .excludePathPatterns(
-            "/swagger-ui/**",
-            "/v3/api-docs/**",
-            "/swagger-resources/**",
-            "/favicon.ico",
-            "/error"
-        );
-  }
+    // 불필요한 swagger 로그 찍히는 부분 설정
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestTimeInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns(
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/favicon.ico",
+                "/error"
+            );
+    }
 
 }
---
+++ src/main/java/com/bbangle/bbangle/config/security/resolver/LocalSecurityArgumentResolver.java
@@ -0,0 +1,49 @@
+package com.bbangle.bbangle.config.security.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * Local 환경에서 @AuthenticationPrincipal 어노테이션이 적용된 Long 타입 파라미터에 Mock seller ID를 자동으로 주입하는 ArgumentResolver
+ * <p>
+ * local 환경에서는 토큰 없이 모든 요청을 허용하기 때문에 SecurityContextHolder에 Authentication이 설정되지 않아
+ *
+ * @AuthenticationPrincipal이 null이 되는 문제를 해결함
+ * <p>
+ * Dev/Production 환경에서는 이 Resolver가 로드되지 않으므로 실제 JWT 토큰에서 추출한 seller ID가 사용됨
+ */
+@Component
+@Profile("local")
+@Slf4j
+public class LocalSecurityArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final Long DEFAULT_USER_ID = 1L;
+
+    /**
+     * @AuthenticationPrincipal 어노테이션이 있는 Long 타입 파라미터를 지원
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticationPrincipal.class)
+            && parameter.getParameterType().equals(Long.class);
+    }
+
+    /**
+     * Local 환경에서 기본값 sellerId 반환
+     */
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        log.debug("[Local] Mock userId={} injected for @AuthenticationPrincipal", DEFAULT_USER_ID);
+        return DEFAULT_USER_ID;
+    }
+}
