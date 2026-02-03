3e2782a2aa75ac164427bdfc3b89c8ddddd321cd
kmindev <kangkm0324@gmail.com>
2026-02-03 23:42:16 +0900

    [Feature] order_item_history id 컬럼 auto_increment로 수정 - 충돌 해결

--- src/main/java/com/bbangle/bbangle/order/domain/OrderItemHistory.java
+++ src/main/java/com/bbangle/bbangle/order/domain/OrderItemHistory.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,7 +25,7 @@ import lombok.NoArgsConstructor;
 public class OrderItemHistory extends CreatedAtBaseEntity {
 
     @Id
-    @Column(name = "id", columnDefinition = "VARCHAR(255)")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
