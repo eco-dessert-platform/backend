d5ec732917e53ca2ead011885305e2e43f1ebbc8
kmindev <kangkm0324@gmail.com>
2026-02-09 23:04:41 +0900

    [Feature] 주문 취소 승인 / 거절 API 구현

--- src/main/java/com/bbangle/bbangle/claim/domain/CancelRequest.java
+++ src/main/java/com/bbangle/bbangle/claim/domain/CancelRequest.java
@@ -1,13 +1,22 @@
 package com.bbangle.bbangle.claim.domain;
 
+import static com.bbangle.bbangle.claim.domain.constant.CancelRequestStatus.APPROVED;
+import static com.bbangle.bbangle.claim.domain.constant.CancelRequestStatus.REJECTED;
+import static com.bbangle.bbangle.claim.domain.constant.CancelRequestStatus.REQUESTED;
+
 import com.bbangle.bbangle.claim.domain.constant.CancelRequestStatus;
+import com.bbangle.bbangle.exception.BbangleErrorCode;
+import com.bbangle.bbangle.exception.BbangleException;
+import com.bbangle.bbangle.order.domain.OrderItem;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,4 +31,34 @@ public class CancelRequest extends Claim {
     @Enumerated(EnumType.STRING)
     private CancelRequestStatus status;
 
+    private String sellerComment;
+
+    @Builder
+    public CancelRequest(
+        OrderItem orderItem,
+        String detailReason,
+        LocalDateTime decidedAt,
+        CancelRequestStatus status
+    ) {
+        super(orderItem, detailReason, decidedAt);
+        this.status = status;
+    }
+
+    public void approve(String reason) {
+        if (status != REQUESTED) {
+            throw new BbangleException(BbangleErrorCode.CLAIM_INVALID_STATUS);
+        }
+        this.status = APPROVED;
+        this.sellerComment = reason;
+        super.decide();
+    }
+
+    public void reject(String reason) {
+        if (status != REQUESTED) {
+            throw new BbangleException(BbangleErrorCode.CLAIM_INVALID_STATUS);
+        }
+        this.status = REJECTED;
+        this.sellerComment = reason;
+        super.decide();
+    }
 }
--- src/main/java/com/bbangle/bbangle/order/domain/OrderItem.java
+++ src/main/java/com/bbangle/bbangle/order/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package com.bbangle.bbangle.order.domain;
 
+import static com.bbangle.bbangle.order.domain.model.OrderStatus.CANCEL_REQUESTED;
 import static com.bbangle.bbangle.order.domain.model.OrderStatus.RETURN_REQUESTED;
 
 import com.bbangle.bbangle.board.domain.Product;
@@ -103,5 +104,19 @@ public class OrderItem extends BaseEntity {
             throw new BbangleException(BbangleErrorCode.ORDER_INVALID_STATUS);
         }
         this.orderStatus = OrderStatus.RETURN_REJECTED;
+    }
+
+    public void cancelApprove() {
+        if (orderStatus != CANCEL_REQUESTED) {
+            throw new BbangleException(BbangleErrorCode.ORDER_INVALID_STATUS);
+        }
+        this.orderStatus = OrderStatus.CANCEL_APPROVED;
+    }
+
+    public void cancelReject() {
+        if (orderStatus != CANCEL_REQUESTED) {
+            throw new BbangleException(BbangleErrorCode.ORDER_INVALID_STATUS);
+        }
+        this.orderStatus = OrderStatus.CANCEL_REJECTED;
     }
 }
