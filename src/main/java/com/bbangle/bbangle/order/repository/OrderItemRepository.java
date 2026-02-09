c4afabb4eb4f157f929fe1a9a6f257753121f740
yeju <bbogaeme@gmail.com>
2026-02-10 00:04:03 +0900

    [feat] OrderItemRepository 수정

--- src/main/java/com/bbangle/bbangle/order/repository/OrderItemRepository.java
+++ src/main/java/com/bbangle/bbangle/order/repository/OrderItemRepository.java
@@ -2,6 +2,7 @@ package com.bbangle.bbangle.order.repository;
 
 import com.bbangle.bbangle.order.domain.OrderItem;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,12 @@ import org.springframework.data.repository.query.Param;
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     List<OrderItem> findByOrderIdAndIdIn(Long orderId, List<Long> ids);
+
+    @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.order WHERE oi.id = :id")
+    Optional<OrderItem> findByIdWithOrder(@Param("id") Long id);
+
+    @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.order WHERE oi.order.id = :orderId AND oi.id IN :ids")
+    List<OrderItem> findByOrderIdAndIdInWithOrder(@Param("orderId") Long orderId, @Param("ids") List<Long> ids);
 
     @Query(value = """
         SELECT COUNT(*)
