c1697da66c64f77d7aa376b8e057fa7517238593
yeju <bbogaeme@gmail.com>
2026-02-11 00:10:59 +0900

    [test] JUnit4 assertThrows를 JUnit5 Assertions로 교체

--- src/test/java/com/bbangle/bbangle/order/seller/service/SellerOrderServiceTest.java
+++ src/test/java/com/bbangle/bbangle/order/seller/service/SellerOrderServiceTest.java
@@ -35,7 +35,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
