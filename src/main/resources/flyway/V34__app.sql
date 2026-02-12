ba6fa159101ac302f8c9bedd36f64eeffe8202dc
Joon Gyu <qkrwnsrb224@gmail.com>
2026-02-13 00:08:26 +0900

    [fix] flyway 파일명 변경

--- src/main/resources/flyway/V34__app.sql
+++
@@ -1,8 +0,0 @@
--- 1. Orders 테이블
-ALTER TABLE orders ADD COLUMN seller_id BIGINT NULL AFTER member_id;
-CREATE INDEX seller_id ON orders (seller_id);
-ALTER TABLE orders ADD CONSTRAINT fk_orders_seller FOREIGN KEY (seller_id) REFERENCES sellers (id);
-
--- 2. Sellers 테이블
-ALTER TABLE sellers ADD CONSTRAINT uk_sellers_store_id UNIQUE (store_id);
-
