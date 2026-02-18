933dcc502b66867c147a8a22c60619573090e5a8
kimbro97 <rlagudwog@gmail.com>
2026-02-18 10:02:02 +0900

    [Fix] V36 마이그레이션 discount_type 전체 행 처리로 수정
    discount_rate > 0 조건으로 인해 discount_rate = 0인 행의
    discount_type이 NULL로 남는 문제 수정
    WHERE 조건을 discount_rate >= 0으로 변경해 모든 행 일괄 처리

--- src/main/resources/flyway/V36__app.sql
+++ src/main/resources/flyway/V36__app.sql
@@ -6,17 +6,11 @@ ALTER TABLE product_board
     ADD COLUMN IF NOT EXISTS is_fresh TINYINT(1) NOT NULL DEFAULT 0,
     ADD COLUMN IF NOT EXISTS production_start_time VARCHAR(20);
 
--- 기존 discount_rate 데이터를 discount_value로 마이그레이션
+-- 기존 discount_rate 데이터를 discount_value로 마이그레이션 (0 포함 전체 행 처리)
 UPDATE product_board
 SET discount_value = discount_rate,
     discount_type = 'RATE'
-WHERE discount_rate > 0;
-
--- discount_rate = 0인 행은 할인 없음으로 처리
-UPDATE product_board
-SET discount_value = 0,
-    discount_type = 'RATE'
-WHERE discount_type IS NULL;
+WHERE discount_rate >= 0;
 
 -- product 테이블에 누락된 컬럼 추가
 ALTER TABLE product
