270d7505a90aaebf43e65b573e0713e08463b0b9
mono0801 <austin0801@naver.com>
2026-03-19 02:21:15 +0900

    [refactor] JPQL 쿼리를 QueryDSL로 리팩토링

---
+++ src/main/java/com/bbangle/bbangle/store/repository/StoreNameRequestQueryDSLRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.bbangle.bbangle.store.repository;
+
+import static com.bbangle.bbangle.store.domain.QStoreNameRequest.storeNameRequest;
+
+import com.bbangle.bbangle.store.domain.StoreNameRequest;
+import com.bbangle.bbangle.store.domain.model.StoreApprovalStatus;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StoreNameRequestQueryDSLRepositoryImpl implements StoreNameRequestQueryDSLRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<StoreApprovalStatus> findActiveRequestsBySellerId(Long sellerId) {
+
+        StoreNameRequest result = queryFactory.selectFrom(storeNameRequest)
+            .where(
+                storeNameRequest.seller.id.eq(sellerId),
+                storeNameRequest.status.in(StoreApprovalStatus.APPROVE, StoreApprovalStatus.PENDING)
+            )
+            .orderBy(
+                new CaseBuilder()
+                    .when(storeNameRequest.status.eq(StoreApprovalStatus.APPROVE)).then(1)
+                    .when(storeNameRequest.status.eq(StoreApprovalStatus.PENDING)).then(2)
+                    .otherwise(3)
+                    .asc()
+            )
+            .fetchFirst();
+
+        return Optional.ofNullable(result)
+            .map(StoreNameRequest::getStatus);
+    }
+}
