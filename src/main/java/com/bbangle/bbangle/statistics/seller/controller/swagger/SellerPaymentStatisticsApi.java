d6b6d0c83a1c5df92e9256264013708ca0b45bc4
kimjongsoo97 <rlwhdtn97@naver.com>
2026-04-09 20:44:34 +0900

    [Feat] 판매자 판매통계(일/주/월)조회 기능 구현 (#630)
    * [Feat] 판매자 판매통계(일/주/월)조회 기능 구현
    
    * [Feat] 판매자 판매통계(일/주/월)조회 기능 구현

---
+++ src/main/java/com/bbangle/bbangle/statistics/seller/controller/swagger/SellerPaymentStatisticsApi.java
@@ -0,0 +1,21 @@
+package com.bbangle.bbangle.statistics.seller.controller.swagger;
+
+import com.bbangle.bbangle.common.dto.SingleResult;
+import com.bbangle.bbangle.statistics.domain.model.StatisticsPeriod;
+import com.bbangle.bbangle.statistics.seller.dto.DailyPaymentAmountResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Tag(name = "Seller Payment Statistics", description = "판매자 결제 통계 API")
+public interface SellerPaymentStatisticsApi {
+
+    @Operation(summary = "판매자 결제 금액 통계 조회")
+    SingleResult<DailyPaymentAmountResponse> getDailyPaymentAmount(
+        @Parameter(description = "판매자 ID") Long sellerId,
+        @Parameter(description = "기준 날짜 (yyyy-MM-dd), 미입력 시 오늘") Optional<LocalDate> date,
+        @Parameter(description = "조회 단위 (DAY, WEEK, MONTH), 미입력 시 DAY") Optional<StatisticsPeriod> period
+    );
+}
