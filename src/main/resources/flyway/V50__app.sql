177f43e9135efe52e22f5dee3aab6863bc8e5ccd
mono0801 <austin0801@naver.com>
2026-04-14 21:47:17 +0900

    [fix] flyway V50 버전 수정

--- src/main/resources/flyway/V50__app.sql
+++ src/main/resources/flyway/V50__app.sql
@@ -1,6 +1,3 @@
--- 1. pagination 핵심
 CREATE INDEX idx_store_application_status_created_id ON store_application (status, created_at, id);
--- 2. accountVerification 서브쿼리
 CREATE INDEX idx_account_verification_seller_verified_id ON account_verifications (seller_id, verified, id DESC);
--- 3. join 성능
 CREATE INDEX idx_store_application_seller_id ON store_application (seller_id);
