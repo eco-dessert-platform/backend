2e5976db1a6de00141c1412dc4ece65bb6965595
kmindev <kangkm0324@gmail.com>
2026-04-18 16:12:09 +0900

    [Chore]  flyway 버전 수정

--- src/main/resources/flyway/V50__app.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX idx_store_application_status_created_id ON store_application (status, created_at, id);
-CREATE INDEX idx_account_verification_seller_verified_id ON account_verifications (seller_id, verified, id DESC);
-CREATE INDEX idx_store_application_seller_id ON store_application (seller_id);
