375cbf26b653d85893eb3ac42a4175791cf3a631
kmindev <kangkm0324@gmail.com>
2026-04-26 19:50:01 +0900

    [Feat] (어드민) RejectionCategory Enum 값 수정
    * [feat] RejectionCategory 항목 변경
      - INVALID_PRICE_CONDITION, INAPPROPRIATE_VEGAN_EXPRESSION, PROHIBITED_LOGO_TEXT 제거
      - OFFICIAL_MATERIAL_CONFUSION, INAPPROPRIATE_EXPRESSION, CONTAINS_ADVERTISING 추가
      - 기존 항목 description 수정
    * [test] BoardTest RejectionCategory 참조 업데이트

--- src/main/java/com/bbangle/bbangle/board/domain/RejectionCategory.java
+++ src/main/java/com/bbangle/bbangle/board/domain/RejectionCategory.java
@@ -7,14 +7,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RejectionCategory {
 
-    ADMIN_JUDGMENT("관리자 판단 부재함"),
-    INAPPROPRIATE_BRAND_NAME("브랜드명 부적절 사용"),
-    INVALID_PRICE_CONDITION("공지사항 가격 조건 위반"),
-    INAPPROPRIATE_VEGAN_EXPRESSION("비거니/비건 표현 부적합"),
-    PROHIBITED_STORE_EXPRESSION("상품명/카테고리 포함 금지 표현"),
-    PROHIBITED_LOGO_TEXT("로고사용 문구 포함"),
-    CONTAINS_CONTACT_INFO("연락처/사이트 포함"),
-    CONTAINS_COMPETITOR_NAME("타 판매자명 공유"),
+    ADMIN_JUDGMENT("관리자 판단 부적합"),
+    INAPPROPRIATE_BRAND_NAME("브랜드명 무단사용"),
+    OFFICIAL_MATERIAL_CONFUSION("공식물 오인 가능"),
+    INAPPROPRIATE_EXPRESSION("비속어/부적절한 표현"),
+    PROHIBITED_STORE_EXPRESSION("상품명/카테고리명 포함"),
+    CONTAINS_ADVERTISING("광고성 문구 포함"),
+    CONTAINS_CONTACT_INFO("연락처/URL 포함"),
+    CONTAINS_COMPETITOR_NAME("타 판매자명 유사"),
     DIRECT_INPUT("직접입력");
 
     private final String description;
