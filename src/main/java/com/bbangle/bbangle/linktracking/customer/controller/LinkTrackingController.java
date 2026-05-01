f49e02e4a9fe249c56973e2d358a2059b8fecaab
yunyechan9893 <dpcks9893@naver.com>
2026-05-01 17:40:58 +0900

    [Refactor] LinkTrackingController Swagger 어노테이션을 API 인터페이스로 분리
    컨트롤러에서 Swagger 어노테이션을 분리하여 LinkTrackingApi 인터페이스로 이동.
    컨트롤러는 매핑 및 비즈니스 로직만 담당하도록 책임을 명확히 함.

--- src/main/java/com/bbangle/bbangle/linktracking/customer/controller/LinkTrackingController.java
+++ src/main/java/com/bbangle/bbangle/linktracking/customer/controller/LinkTrackingController.java
@@ -1,12 +1,10 @@
 package com.bbangle.bbangle.linktracking.customer.controller;
 
-import com.bbangle.bbangle.linktracking.customer.service.command.LinkTrackingCommand;
-import com.bbangle.bbangle.linktracking.customer.facade.LinkTrackingFacade;
 import com.bbangle.bbangle.linktracking.customer.controller.mapper.LinkTrackingCommandMapper;
+import com.bbangle.bbangle.linktracking.customer.controller.swagger.LinkTrackingApi;
+import com.bbangle.bbangle.linktracking.customer.facade.LinkTrackingFacade;
+import com.bbangle.bbangle.linktracking.customer.service.command.LinkTrackingCommand;
 import com.bbangle.bbangle.linktracking.domain.LinkChannel;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
@@ -17,19 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "LinkTracking", description = "외부 유입 링크 추적")
 @RestController
 @RequestMapping("/api/v1/link")
 @RequiredArgsConstructor
-public class LinkTrackingController {
+public class LinkTrackingController implements LinkTrackingApi {
 
     private final LinkTrackingFacade linkTrackingFacade;
     private final LinkTrackingCommandMapper linkTrackingCommandMapper;
 
-    @Operation(summary = "추적 링크 리다이렉트", description = "조회수를 기록한 뒤 실제 목적지로 302 리다이렉트")
     @GetMapping("/{channel}")
     public ResponseEntity<Void> redirect(
-        @Parameter(description = "유입 채널", example = "INSTAGRAM")
         @PathVariable("channel") LinkChannel channel,
         HttpServletRequest request
     ) {
