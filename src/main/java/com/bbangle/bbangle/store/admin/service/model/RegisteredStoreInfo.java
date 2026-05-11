d641d454c8d430d7266a9cecf76df39697405058
kmindev <kangkm0324@gmail.com>
2026-05-11 23:45:11 +0900

    [Fix] (어드민) phoneNumberVO/emailVO null 체크 추가로 NPE 수정

--- src/main/java/com/bbangle/bbangle/store/admin/service/model/RegisteredStoreInfo.java
+++ src/main/java/com/bbangle/bbangle/store/admin/service/model/RegisteredStoreInfo.java
@@ -2,6 +2,8 @@ package com.bbangle.bbangle.store.admin.service.model;
 
 import com.bbangle.bbangle.seller.domain.Seller;
 import com.bbangle.bbangle.store.domain.Store;
+import com.bbangle.bbangle.store.domain.model.EmailVO;
+import com.bbangle.bbangle.store.domain.model.PhoneNumberVO;
 
 public record RegisteredStoreInfo(
     long storeId,
@@ -20,14 +22,16 @@ public record RegisteredStoreInfo(
             ? store.getOriginAddressLine() + " " + detail
             : store.getOriginAddressLine();
 
+        PhoneNumberVO phoneNumberVO = store.getPhoneNumberVO();
+        EmailVO emailVO = store.getEmailVO();
         return new RegisteredStoreInfo(
             store.getId(),
             store.getName(),
             store.getIdentifier(),
             seller != null ? seller.getName() : null,
-            store.getPhoneNumberVO().getPhoneNumber(),
-            store.getPhoneNumberVO().getSubPhoneNumber(),
-            store.getEmailVO().getEmail(),
+            phoneNumberVO != null ? phoneNumberVO.getPhoneNumber() : null,
+            phoneNumberVO != null ? phoneNumberVO.getSubPhoneNumber() : null,
+            emailVO != null ? emailVO.getEmail() : null,
             address
         );
     }
